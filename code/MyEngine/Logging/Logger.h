c4177c851db036abdab8b982db316b7951922745
Louis Herman <louisherman@live.be>
2022-09-12 03:06:14 +0200

    ADD: ToStringPrint(..)

--- code/MyEngine/Logging/Logger.h
+++ code/MyEngine/Logging/Logger.h
@@ -11,6 +11,15 @@ namespace MyEngine
 			static std::string GetLogLine(const std::string& logMessage); //returns: [13:20:13]: MyMessage/n
 
 			static void Print(const std::string& logMessage); //print to console: [13:20:13]: MyMessage/n
+
+			template <typename T>
+			static void ToStringPrint(const T& value);
 		};
+
+		template <typename T>
+		void Logger::ToStringPrint(const T& value)
+		{
+			Print(std::to_string(value));
+		}
 	}
 }
