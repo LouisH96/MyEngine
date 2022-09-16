42d9e6c0b68af9f814ca1e1235b0530bdfdf2133
Louis Herman <louisherman@live.be>
2022-09-16 05:06:35 +0200

    ADD: Math class with constants

---
+++ code/MyEngine/Math/Math.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace MyEngine
+{
+	class Math
+	{
+	public:
+		static constexpr float PI = 3.14159265358979323846f;
+		static constexpr float PI_DIV2 = 1.57079632679489661923;
+		static constexpr float TO_RAD = PI / 180.f;
+
+	};
+}
