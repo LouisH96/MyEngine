31b78cb9318fa99bc047510f0380f4b3801f7fe4
Louis Herman <louisherman@live.be>
2023-03-05 19:31:56 +0100

    ADD: sphere definition to Math

---
+++ code/MyEngine/Math/Sphere.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "Sphere.h"
+
+Math::Sphere::Sphere(const Float3& center, float radius)
+	: m_Center{ center }
+	, m_Radius{ radius }
+{
+}
---
+++ code/MyEngine/Math/Sphere.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Float3.h"
+
+namespace MyEngine
+{
+	namespace Math
+	{
+		class Sphere
+		{
+		public:
+			Sphere(const Float3& center, float radius);
+
+			const Float3& GetCenter() const { return m_Center; }
+			float GetRadius() const { return m_Radius; }
+
+		private:
+			Float3 m_Center;
+			float m_Radius;
+		};
+	}
+}
