326a3b5c8fb3fcb8ff2f3027ed6c2953c7ffec63
Louis Herman <louisherman@live.be>
2023-03-11 19:50:35 +0100

    FIX: test.cpp using old Logger namespace

--- code/UnitTest/test.cpp
+++ code/UnitTest/test.cpp
@@ -1,9 +1,9 @@
 #include "pch.h"
-#include "../MyEngine/Logging/Logger.h"
+#include "../MyEngine/Debug/Logger.h"
 
 
 TEST(TestCaseName, TestName) {
-	using Logger = MyEngine::Logging::Logger;
+	using Logger = MyEngine::Debug::Logger;
 
 	constexpr int expected{ 12 + 5 };
 	const int result = static_cast<int>(Logger::GetLogLine("Hallo").length());
