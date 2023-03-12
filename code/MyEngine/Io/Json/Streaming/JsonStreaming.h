4c39662a42d0284489ef4acb745241d12b43d9d9
Louis Herman <louisherman@live.be>
2023-03-12 21:51:51 +0100

    ADD: functionality for stream a json (needs lots of improvements)

---
+++ code/MyEngine/Io/Json/Streaming/JsonStreaming.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <fstream>
+#include <stack>
+
+#include "Io/Json/JsonElement.h"
+
+//need lots of cleaning
+//performance can improve a lot
+namespace MyEngine
+{
+	namespace Json
+	{
+		enum class StreamState
+		{
+			Root,
+			InObject,
+			Unknown
+		};
+
+		class JsonStreaming
+		{
+		public:
+			JsonStreaming(const std::wstring& path);
+			~JsonStreaming();
+
+
+			std::string GetNextProperty();
+			void GetInNextObject();
+			void GetInArray();
+			Io::Json::ElementType FindNextElementType();
+
+			std::string GetNextStringValue();
+			double GetNextNumberValue();
+			void SkipNextScope();
+			void SkipNextNull();
+			void SkipNextNumber();
+			void SkipNextPropertyValue();
+			void SkipNextProperty();
+			void GetOutOfScope();
+			void FindProperty(const std::string& propertyName);
+
+			bool IsEndOfObject();
+			bool IsEndOfArray();
+
+		private:
+			std::ifstream m_Stream;
+			StreamState m_State;
+			std::stack<char> m_Scopes{};
+
+			static char FindNextSign(std::ifstream& stream);
+			static std::string GetNextString(std::ifstream& stream);
+			static void SkipWhiteSpace(std::ifstream& stream);
+		};
+	}
+}
