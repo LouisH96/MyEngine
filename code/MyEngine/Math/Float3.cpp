468298459476b7bd07bff24473295839afe1fc38
Louis Herman <louisherman@live.be>
2023-03-28 19:58:01 +0200

    EDIT: remove int operations in float3

--- code/MyEngine/Math/Float3.cpp
+++ code/MyEngine/Math/Float3.cpp
@@ -3,9 +3,7 @@
 
 Math::Float3::Float3() : x{ 0 }, y{ 0 }, z{ 0 } {}
 Math::Float3::Float3(float x, float y, float z) : x{ x }, y{ y }, z{ z } {}
-Math::Float3::Float3(int x, int y, int z) : x{ static_cast<float>(x) }, y{ static_cast<float>(y) }, z{ static_cast<float>(z) } {}
 Math::Float3::Float3(float all) : x{ all }, y{ all }, z{ all } {}
-Math::Float3::Float3(int all) : x{ static_cast<float>(all) }, y{ static_cast<float>(all) }, z{ static_cast<float>(all) } {}
 
 Math::Float3 Math::Float3::operator+(const Float3& r) const { return { x + r.x, y + r.y, z + r.z }; }
 Math::Float3 Math::Float3::operator-(const Float3& r) const { return { x - r.x, y - r.y, z - r.z }; }
@@ -13,20 +11,12 @@ Math::Float3 Math::Float3::operator+(const float& r) const { return { x + r, y +
 Math::Float3 Math::Float3::operator-(const float& r) const { return { x - r, y - r, z - r }; }
 Math::Float3 Math::Float3::operator*(const float& r) const { return { x * r, y * r, z * r }; }
 Math::Float3 Math::Float3::operator/(const float& r) const { return { x / r, y / r, z / r }; }
-Math::Float3 Math::Float3::operator+(const int& r) const { return { x + static_cast<float>(r), y + static_cast<float>(r), z + static_cast<float>(r) }; }
-Math::Float3 Math::Float3::operator-(const int& r) const { return { x - static_cast<float>(r), y - static_cast<float>(r), z - static_cast<float>(r) }; }
-Math::Float3 Math::Float3::operator*(const int& r) const { return { x * static_cast<float>(r), y * static_cast<float>(r), z * static_cast<float>(r) }; }
-Math::Float3 Math::Float3::operator/(const int& r) const { return { x / static_cast<float>(r), y / static_cast<float>(r), z / static_cast<float>(r) }; }
 void Math::Float3::operator+=(const Float3& r) { x += r.x; y += r.y; z += r.z; }
 void Math::Float3::operator-=(const Float3& r) { x -= r.x; y -= r.y; z -= r.z; }
 void Math::Float3::operator+=(const float& r) { x += r; y += r; z += r; }
 void Math::Float3::operator-=(const float& r) { x -= r; y -= r; z -= r; }
 void Math::Float3::operator*=(const float& r) { x *= r; y *= r; z *= r; }
 void Math::Float3::operator/=(const float& r) { x /= r; y /= r; z /= r; }
-void Math::Float3::operator+=(const int& r) { x += static_cast<float>(r); y += static_cast<float>(r); z += static_cast<float>(r); }
-void Math::Float3::operator-=(const int& r) { x -= static_cast<float>(r); y -= static_cast<float>(r); z -= static_cast<float>(r); }
-void Math::Float3::operator*=(const int& r) { x *= static_cast<float>(r); y *= static_cast<float>(r); z *= static_cast<float>(r); }
-void Math::Float3::operator/=(const int& r) { x /= static_cast<float>(r); y /= static_cast<float>(r); z /= static_cast<float>(r); }
 
 Math::Float3 Math::Float3::operator-() const
 {
--- code/MyEngine/Math/Float3.h
+++ code/MyEngine/Math/Float3.h
@@ -8,9 +8,7 @@ namespace MyEngine
 		{
 			Float3();
 			Float3(float x, float y, float z);
-			Float3(int x, int y, int z);
 			explicit Float3(float all);
-			explicit Float3(int all);
 			~Float3() = default;
 			Float3(const Float3& other) = default;
 			Float3(Float3&& other) noexcept = default;
@@ -23,20 +21,12 @@ namespace MyEngine
 			Float3 operator-(const float& r) const;
 			Float3 operator*(const float& r) const;
 			Float3 operator/(const float& r) const;
-			Float3 operator+(const int& r) const;
-			Float3 operator-(const int& r) const;
-			Float3 operator*(const int& r) const;
-			Float3 operator/(const int& r) const;
 			void operator+=(const Float3& r);
 			void operator-=(const Float3& r);
 			void operator+=(const float& r);
 			void operator-=(const float& r);
 			void operator*=(const float& r);
 			void operator/=(const float& r);
-			void operator+=(const int& r);
-			void operator-=(const int& r);
-			void operator*=(const int& r);
-			void operator/=(const int& r);
 			Float3 operator-() const;
 
 			float Get(int i) const;
