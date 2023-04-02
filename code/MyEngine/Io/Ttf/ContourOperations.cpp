4eb0d6d606e0de740adc6f6c151368ea164493fc
Louis Herman <louisherman@live.be>
2023-04-02 23:17:09 +0200

    ADD: ContourOperations::GetIntersections(..)

--- code/MyEngine/Io/Ttf/ContourOperations.cpp
+++ code/MyEngine/Io/Ttf/ContourOperations.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "ContourOperations.h"
 
+#include "DataStructures/DsUtils.h"
+
 bool Io::Ttf::ContourOperations::Segment::IsLinear() const
 {
 	return p1.x == INFINITY || p1.y == INFINITY;
@@ -112,6 +114,15 @@ Array<Math::Float2> Io::Ttf::ContourOperations::ToPoints(const Array<Segment>& s
 	return points;
 }
 
+Array<Array<Io::Ttf::ContourOperations::Segment>> Io::Ttf::ContourOperations::ToSegments(
+	const Array<Array<TtfPoint>>& contourPoints)
+{
+	Array<Array<Segment>> segments{ contourPoints.GetSize() };
+	for (int i = 0; i < contourPoints.GetSize(); i++)
+		segments[i] = ToSegments(contourPoints[i]);
+	return segments;
+}
+
 Array<Io::Ttf::ContourOperations::Segment> Io::Ttf::ContourOperations::ToSegments(const Array<TtfPoint>& contourPoints)
 {
 	int nrSegments = 0;
@@ -164,6 +175,19 @@ Array<Io::Ttf::ContourOperations::Segment> Io::Ttf::ContourOperations::ToSegment
 	return segments;
 }
 
+Array<float> Io::Ttf::ContourOperations::GetIntersectionsX(const Array<Segment>& segments, float height)
+{
+	std::vector<float> intersections{};
+	for (int i = 0; i < segments.GetSize(); i++)
+	{
+		if (segments[i].IsLinear())
+			AddIntersectionsLinear(segments[i], height, intersections);
+		else
+			AddIntersectionsCurve(segments[i], height, intersections);
+	}
+	return DsUtils::ToArray(intersections);
+}
+
 Math::Float2 Io::Ttf::ContourOperations::CalculatePoint(const Math::Float2& p0, const Math::Float2& p1,
 	const Math::Float2& p2, float alpha)
 {
@@ -179,4 +203,43 @@ Math::Float2 Io::Ttf::ContourOperations::CalculatePoint(const Segment& segment, 
 	return segment.p0 * (invAlpha * invAlpha)
 		+ segment.p1 * (2 * invAlpha * alpha)
 		+ segment.p2 * (alpha * alpha);
+}
+
+void Io::Ttf::ContourOperations::AddIntersectionsCurve(const Segment& curve, float height,
+	std::vector<float>& intersections)
+{
+	//https://www.wolframalpha.com/input?i=solve+t%2C+y+%3D+a+*+%281-t%29%5E2+%2B+b+*+2+*%281-t%29*t+%2B+c+*+t%5E2
+	const float a{ curve.p0.y };
+	const float b{ curve.p1.y };
+	const float c{ curve.p2.y };
+	const float divisor{ a - 2 * b + c };
+	if (divisor == 0)
+		return;
+	const float sqrt{ sqrtf(-a * c + a * height + b * b - 2 * b * height + c * height) };
+	const float result1{ (sqrt + a - b) / divisor };
+	const float result2{ -(sqrt - a + b) / divisor };
+	if (result1 >= 0 && result1 <= 1)
+	{
+		const Math::Float2 point{ CalculatePoint(curve, result1) };
+		intersections.push_back(point.x);
+	}
+	if (result2 >= 0 && result2 <= 1)
+	{
+		const Math::Float2 point{ CalculatePoint(curve, result2) };
+		intersections.push_back(point.x);
+	}
+}
+
+void Io::Ttf::ContourOperations::AddIntersectionsLinear(const Segment& linear, float height,
+	std::vector<float>& intersections)
+{
+	const float divisor{ linear.p2.y - linear.p0.y };
+	if (divisor == 0)
+		return;
+	const float result{ (height - linear.p0.y) / divisor };
+	if (result >= 0 && result <= 1)
+	{
+		const Math::Float2 point{ linear.p0 + (linear.p2 - linear.p0) * result };
+		intersections.push_back(point.x);
+	}
 }
--- code/MyEngine/Io/Ttf/ContourOperations.h
+++ code/MyEngine/Io/Ttf/ContourOperations.h
@@ -25,11 +25,17 @@ namespace MyEngine
 				static Array<Math::Float2> ToPoints(const Array<TtfPoint>& contourPoints, int nrPointsPerSegment = 10);
 				static Array<Math::Float2> ToPoints(const Segment& segment, int nrPoints = 10);
 				static Array<Math::Float2> ToPoints(const Array<Segment>& segments, int nrPointsForCurve = 10);
+				static Array<Array<Segment>> ToSegments(const Array<Array<TtfPoint>>& contourPoints);
 				static Array<Segment> ToSegments(const Array<TtfPoint>& contourPoints);
+
+				static Array<float> GetIntersectionsX(const Array<Segment>& segments, float height);
 
 			private:
 				static Math::Float2 CalculatePoint(const Math::Float2& p0, const Math::Float2& p1, const Math::Float2& p2, float alpha);
 				static Math::Float2 CalculatePoint(const Segment& segment, float alpha);
+
+				static void AddIntersectionsCurve(const Segment& curve, float height, std::vector<float>& intersections);
+				static void AddIntersectionsLinear(const Segment& linear, float height, std::vector<float>& intersections);
 			};
 		}
 	}
