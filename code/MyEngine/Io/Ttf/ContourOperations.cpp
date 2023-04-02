b0e5982b1fc76a76c644c1bc44749bf93de1a452
Louis Herman <louisherman@live.be>
2023-04-02 20:01:27 +0200

    ADD: segment

--- code/MyEngine/Io/Ttf/ContourOperations.cpp
+++ code/MyEngine/Io/Ttf/ContourOperations.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
 #include "ContourOperations.h"
 
+bool Io::Ttf::ContourOperations::Segment::IsLinear() const
+{
+	return p1.x == INFINITY || p1.y == INFINITY;
+}
+
 Array<Math::Float2> Io::Ttf::ContourOperations::ToPoints(const Array<TtfPoint>& contourPoints, int nrPointsPerSegment)
 {
 	if (nrPointsPerSegment < 2)
@@ -59,6 +64,106 @@ Array<Math::Float2> Io::Ttf::ContourOperations::ToPoints(const Array<TtfPoint>& 
 	return points;
 }
 
+Array<Math::Float2> Io::Ttf::ContourOperations::ToPoints(const Segment& segment, int nrPoints)
+{
+	if (segment.IsLinear())
+	{
+		Array<Math::Float2> points{ 2 };
+		points[0] = { segment.p0 };
+		points[1] = { segment.p2 };
+		return points;
+	}
+	if (nrPoints < 2)
+	{
+		Logger::PrintError("Need at least 2 points for segment");
+		return {};
+	}
+	Array<Math::Float2> points{ nrPoints };
+	const float alphaStep{ 1.f / (nrPoints - 1) };
+	points[0] = segment.p0;
+	for (int i = 1; i < nrPoints; i++)
+		points[i] = CalculatePoint(segment, i * alphaStep);
+	return points;
+}
+
+Array<Math::Float2> Io::Ttf::ContourOperations::ToPoints(const Array<Segment>& segments, int nrPointsForCurve)
+{
+	int nrPoints = 1;
+	for (int i = 0; i < segments.GetSize(); i++)
+		if (segments[i].IsLinear())
+			nrPoints += 1;
+		else
+			nrPoints += nrPointsForCurve - 1;
+
+	Array<Math::Float2> points{ nrPoints };
+	int iPoint = 0;
+	for (int iSegment = 0; iSegment < segments.GetSize(); iSegment++)
+	{
+		if (segments[iSegment].IsLinear())
+			points[iPoint++] = segments[iSegment].p0;
+		else
+		{
+			const Array<Math::Float2> segmentPoints{ ToPoints(segments[iSegment], nrPointsForCurve) };
+			for (int iSegmentPoint = 0; iSegmentPoint < segmentPoints.GetSize() - 1; iSegmentPoint++)
+				points[iPoint++] = segmentPoints[iSegmentPoint];
+		}
+	}
+	points[points.GetSize() - 1] = points[0];
+	return points;
+}
+
+Array<Io::Ttf::ContourOperations::Segment> Io::Ttf::ContourOperations::ToSegments(const Array<TtfPoint>& contourPoints)
+{
+	int nrSegments = 0;
+	for (int i = 1; i < contourPoints.GetSize(); i++)
+	{
+		if (contourPoints[i].isOnCurve)
+		{
+			if (contourPoints[i - 1].isOnCurve)
+				nrSegments++;
+		}
+		else
+			nrSegments++;
+	}
+	if (contourPoints[contourPoints.GetSize() - 1].isOnCurve)
+		nrSegments++;
+
+	//
+	Array<Segment> segments{ nrSegments };
+	int iSegment = 0;
+	for (int i = 1; i < contourPoints.GetSize();)
+	{
+		if (contourPoints[i].isOnCurve)
+		{
+			if (contourPoints[i - 1].isOnCurve)
+			{
+				segments[iSegment++] = Segment{
+					contourPoints[i - 1].position, {INFINITY, INFINITY},
+					contourPoints[i].position };
+			}
+			i++;
+		}
+		else
+		{
+			const Math::Float2 prevPoint = iSegment == 0 ? Math::Float2{ contourPoints[0].position } : segments[iSegment - 1].p2;
+			const Math::Float2 controlPoint = contourPoints[i].position;
+			Math::Float2 nextPoint;
+			if (contourPoints[i + 1].isOnCurve)
+			{
+				nextPoint = contourPoints[i + 1].position;
+				i += 2;
+			}
+			else
+			{
+				nextPoint = (controlPoint + Math::Float2{ contourPoints[i + 1].position }) / 2;
+				i++;
+			}
+			segments[iSegment++] = { prevPoint, controlPoint, nextPoint };
+		}
+	}
+	return segments;
+}
+
 Math::Float2 Io::Ttf::ContourOperations::CalculatePoint(const Math::Float2& p0, const Math::Float2& p1,
 	const Math::Float2& p2, float alpha)
 {
@@ -66,4 +171,12 @@ Math::Float2 Io::Ttf::ContourOperations::CalculatePoint(const Math::Float2& p0, 
 	return p0 * (invAlpha * invAlpha)
 		+ p1 * (2 * invAlpha * alpha)
 		+ p2 * (alpha * alpha);
+}
+
+Math::Float2 Io::Ttf::ContourOperations::CalculatePoint(const Segment& segment, float alpha)
+{
+	const float invAlpha = 1 - alpha;
+	return segment.p0 * (invAlpha * invAlpha)
+		+ segment.p1 * (2 * invAlpha * alpha)
+		+ segment.p2 * (alpha * alpha);
 }
--- code/MyEngine/Io/Ttf/ContourOperations.h
+++ code/MyEngine/Io/Ttf/ContourOperations.h
@@ -11,10 +11,25 @@ namespace MyEngine
 			class ContourOperations
 			{
 			public:
+				//---| Types |---
+				struct Segment
+				{
+					Math::Float2 p0;
+					Math::Float2 p1; //infinity if linear segment
+					Math::Float2 p2;
+
+					bool IsLinear() const;
+				};
+
+				//---| Operations |---
 				static Array<Math::Float2> ToPoints(const Array<TtfPoint>& contourPoints, int nrPointsPerSegment = 10);
+				static Array<Math::Float2> ToPoints(const Segment& segment, int nrPoints = 10);
+				static Array<Math::Float2> ToPoints(const Array<Segment>& segments, int nrPointsForCurve = 10);
+				static Array<Segment> ToSegments(const Array<TtfPoint>& contourPoints);
 
 			private:
 				static Math::Float2 CalculatePoint(const Math::Float2& p0, const Math::Float2& p1, const Math::Float2& p2, float alpha);
+				static Math::Float2 CalculatePoint(const Segment& segment, float alpha);
 			};
 		}
 	}
