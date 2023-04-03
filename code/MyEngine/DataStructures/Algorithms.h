1010665ccefe1151fad66e10ecb234a2fda31893
Louis Herman <louisherman@live.be>
2023-04-03 22:29:02 +0200

    ADD: simple sort algorithm

--- code/MyEngine/DataStructures/Algorithms.h
+++ code/MyEngine/DataStructures/Algorithms.h
@@ -18,6 +18,9 @@ namespace MyEngine
 
 			template<typename T>
 			static void GetMinAndMax(const Array<T>& array, T& min, T& max);
+
+			template<typename T>
+			static void SortSmallToBig(Array<T>& array);
 		};
 
 		template <typename T>
@@ -39,6 +42,27 @@ namespace MyEngine
 			{
 				if (array[i] < min) min = array[i];
 				if (array[i] > max) max = array[i];
+			}
+		}
+
+		template <typename T>
+		void Algorithms::SortSmallToBig(Array<T>& array)
+		{
+			for(int iFirst = 0; iFirst < array.GetSize()-1; iFirst++)
+			{
+				T value{ array[iFirst] };
+				int valueIdx{ iFirst };
+				for(int iSecond = iFirst+1; iSecond < array.GetSize(); iSecond++)
+				{
+					if(array[iSecond] < value)
+					{
+						value = array[iSecond];
+						valueIdx = iSecond;
+					}
+				}
+				if (valueIdx == iFirst) continue;
+				array[valueIdx] = array[iFirst];
+				array[iFirst] = value;
 			}
 		}
 	}
