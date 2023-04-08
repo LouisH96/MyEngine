fa9c26caa2b24270dfd6b31b79edab8eeb9b24ad
Louis Herman <louisherman@live.be>
2023-04-08 16:56:41 +0200

    ADD: more Image::SetColor(..) functions

--- code/MyEngine/Image.cpp
+++ code/MyEngine/Image.cpp
@@ -31,3 +31,28 @@ void Rendering::Image::SetColor(int col, int row, uint8_t r, uint8_t g, uint8_t 
 	pColor[2] = b;
 	pColor[3] = a;
 }
+
+void Rendering::Image::SetColor(int col, int row, const Math::Float3& color, float a) const
+{
+	uint8_t* pColor{ &m_pData[(row * m_Width + col) * 4] };
+	pColor[0] = static_cast<uint8_t>(color.x * 255.f);
+	pColor[1] = static_cast<uint8_t>(color.y * 255.f);
+	pColor[2] = static_cast<uint8_t>(color.z * 255.f);
+	pColor[3] = static_cast<uint8_t>(a * 255.f);
+}
+
+void Rendering::Image::SetRowColor(int row, const Math::Float3& color, float a) const
+{
+	const uint8_t r8{ static_cast<uint8_t>(color.x * 255.f) };
+	const uint8_t g8{ static_cast<uint8_t>(color.y * 255.f) };
+	const uint8_t b8{ static_cast<uint8_t>(color.z * 255.f) };
+	const uint8_t a8{ static_cast<uint8_t>(a * 255.f) };
+	uint8_t* pChannel{ &m_pData[row * m_Width * 4] };
+	for(int i = 0; i < m_Width; i++)
+	{
+		*pChannel++ = r8;
+		*pChannel++ = g8;
+		*pChannel++ = b8;
+		*pChannel++ = a8;
+	}
+}
--- code/MyEngine/Image.h
+++ code/MyEngine/Image.h
@@ -12,7 +12,10 @@ namespace MyEngine
 
 			void Clear();
 			bool IsCleared() const;
+
 			void SetColor(int col, int row, uint8_t r, uint8_t g, uint8_t b, uint8_t a) const;
+			void SetColor(int col, int row, const Math::Float3& color, float a = 1.f) const;
+			void SetRowColor(int row, const Math::Float3& color, float a = 1.f) const;
 
 			int GetWidth() const { return m_Width; }
 			int GetHeight() const { return m_Height; }
