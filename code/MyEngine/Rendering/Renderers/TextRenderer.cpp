27eb380e9a255aa3a1b8b90374c6b29ab1997d03
Louis Herman <louisherman@live.be>
2023-04-08 17:16:19 +0200

    EDIT: TextRenderer can show multiple characters

--- code/MyEngine/Rendering/Renderers/TextRenderer.cpp
+++ code/MyEngine/Rendering/Renderers/TextRenderer.cpp
@@ -6,34 +6,42 @@
 #include "Image.h"
 #include "App/Resources.h"
 #include "Io/Ttf/ContourOperations.h"
+#include "Io/Ttf/FontRasterizer.h"
 #include "Io/Ttf/TtfReader.h"
 
 Rendering::TextRenderer::TextRenderer(Gpu& gpu, Game::Camera& camera)
 	: m_pTextureRenderer(
 		new TextureRenderer<V_PosUv, CB_CamMat>(gpu, camera, App::Resources::GetGlobalShaderPath(L"unlitTexture.hlsl")))
+	, m_Gpu(gpu)
 {
-	Array<V_PosUv> vertices{ 6 };
-	vertices[0] = { {0,1,0},{0,0} };
-	vertices[1] = { {1,1,0},{1,0} };
-	vertices[2] = { {1,0,0},{1,1} };
-	vertices[3] = { {0,1,0},{0,0} };
-	vertices[4] = { {1,0,0},{1,1} };
-	vertices[5] = { {0,0,0},{0,1} };
-	Mesh* pMesh = Mesh::Create(gpu, vertices);
-
-	auto path = App::Resources::GetGlobalResourcePath(L"Fonts\\Envy Code R.ttf");
-	std::ifstream stream{ path, std::ios::binary };
-	Io::TtfReader r{ stream };
-	Image* pImage = Io::Ttf::ContourOperations::MakeImage(r, '6', 64 , 64 );
-	Texture* pTexture = new Texture(gpu, pImage);
-	delete pImage;
-
-	m_pTextureRenderer->AddMesh(pMesh, pTexture);
 }
+
 
 Rendering::TextRenderer::~TextRenderer()
 {
 	delete m_pTextureRenderer;
+}
+
+void Rendering::TextRenderer::AddCharacterXy(const Math::Float3& offset, const Math::Float2& size,
+	const Io::Ttf::FontRasterizer& rasterizer) const
+{
+	const float left{ offset.x };
+	const float bot{ offset.y };
+	const float right{ left + size.x };
+	const float top{ bot + size.y };
+	const float z{ offset.z };
+	Array<V_PosUv> vertices{ 6 };
+	vertices[0] = { {left,top,z},{0,0} };
+	vertices[1] = { {right,top,z},{1,0} };
+	vertices[2] = { {right,bot,z},{1,1} };
+	vertices[3] = { {left,top,z},{0,0} };
+	vertices[4] = { {right,bot,z},{1,1} };
+	vertices[5] = { {left,bot,z},{0,1} };
+	Mesh* pMesh = Mesh::Create(m_Gpu, vertices);
+	Image* pImage = rasterizer.MakeImage();
+	Texture* pTexture = new Texture(m_Gpu, pImage);
+	delete pImage;
+	m_pTextureRenderer->AddMesh(pMesh, pTexture);
 }
 
 void Rendering::TextRenderer::Render() const
--- code/MyEngine/Rendering/Renderers/TextRenderer.h
+++ code/MyEngine/Rendering/Renderers/TextRenderer.h
@@ -5,6 +5,14 @@
 
 namespace MyEngine
 {
+	namespace Io
+	{
+		namespace Ttf
+		{
+			class FontRasterizer;
+		}
+	}
+
 	namespace Game
 	{
 		class Camera;
@@ -20,10 +28,12 @@ namespace MyEngine
 			TextRenderer(Gpu& gpu, Game::Camera& camera);
 			~TextRenderer();
 
+			void AddCharacterXy(const Math::Float3& offset, const Math::Float2& size, const Io::Ttf::FontRasterizer& rasterizer) const;
 			void Render() const;
 
 		private:
 			TextureRenderer<V_PosUv, CB_CamMat>* m_pTextureRenderer;
+			Gpu& m_Gpu;
 		};
 	}
 }
