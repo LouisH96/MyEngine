2bc980f83f6039cdb560f069f069d39e53028d59
Louis Herman <louisherman@live.be>
2023-05-15 22:43:22 +0200

    ADD(AsciiWriter): way to write with leading tabs

--- code/MyEngine/Io/Ascii/AsciiWriter.cpp
+++ code/MyEngine/Io/Ascii/AsciiWriter.cpp
@@ -5,3 +5,9 @@ void Io::Ascii::AsciiWriter::Tabs(std::ostream& stream, int nrTabs)
 {
 	for (int i = 0; i < nrTabs; i++) stream << '\t';
 }
+
+Io::Ascii::AsciiWriter::AsciiWriter(std::ostream& stream, int nrTabs)
+	: m_Stream{ stream }
+	, m_NrTabs{ nrTabs }
+{
+}
--- code/MyEngine/Io/Ascii/AsciiWriter.h
+++ code/MyEngine/Io/Ascii/AsciiWriter.h
@@ -10,7 +10,23 @@ namespace MyEngine
 			{
 			public:
 				static void Tabs(std::ostream& stream, int nrTabs);
+
+				explicit AsciiWriter(std::ostream& stream, int nrTabs = 0);
+
+				template<typename T>
+				std::ostream& operator<<(T& value);
+
+			private:
+				std::ostream& m_Stream;
+				int m_NrTabs;
 			};
+
+			template <typename T>
+			std::ostream& AsciiWriter::operator<<(T& value)
+			{
+				Tabs(m_Stream, m_NrTabs);
+				return m_Stream << value;
+			}
 		}
 	}
 }
