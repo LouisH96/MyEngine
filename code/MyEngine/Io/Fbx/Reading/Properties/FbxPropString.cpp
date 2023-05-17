2536e0146c7267cebf74a1b4db78d36b79d88e49
Louis Herman <louisherman@live.be>
2023-05-17 23:11:03 +0200

    EDIT(FbxPropString): cut string at \0

--- code/MyEngine/Io/Fbx/Reading/Properties/FbxPropString.cpp
+++ code/MyEngine/Io/Fbx/Reading/Properties/FbxPropString.cpp
@@ -4,21 +4,21 @@
 
 using namespace Io::Binary;
 
-MyEngine::Io::Fbx::Reading:: FbxPropString::FbxPropString(std::istream& stream)
-    : m_String(Read(stream))
+MyEngine::Io::Fbx::Reading::FbxPropString::FbxPropString(std::istream& stream)
+	: m_String(Read(stream))
 {
 }
 
 void MyEngine::Io::Fbx::Reading::FbxPropString::Print(bool compact, int nrTabs) const
 {
-    BeginPrint(nrTabs);
-    std::cout << "[string] " << m_String << std::endl;
+	BeginPrint(nrTabs);
+	std::cout << "[string] " << m_String << std::endl;
 }
 
 std::string MyEngine::Io::Fbx::Reading::FbxPropString::Read(std::istream& stream)
 {
-    const unsigned size{ LittleEndianReader::Uint32(stream) };
-    std::string string( size, ' ' );
-    stream.read(&string[0], size);
-    return string;
+	const unsigned size{ LittleEndianReader::Uint32(stream) };
+	std::string string(size, ' ');
+	stream.read(&string[0], size);
+	return string.substr(0, string.find('\0'));
 }
