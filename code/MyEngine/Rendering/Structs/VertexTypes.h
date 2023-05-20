186b47cc8b27a7212500cf6271a59c4f70fdff32
Louis Herman <louisherman@live.be>
2023-05-20 21:26:02 +0200

    CLEAN(VertexTypes): removed unnecessary namespace

--- code/MyEngine/Rendering/Structs/VertexTypes.h
+++ code/MyEngine/Rendering/Structs/VertexTypes.h
@@ -9,8 +9,8 @@ namespace MyEngine
 	{
 		struct V_Pos2Uv
 		{
-			Math::Float2 pos;
-			Math::Float2 uv;
+			Float2 pos;
+			Float2 uv;
 
 			static const InputLayout::Element ELEMENTS[];
 			static constexpr int NR_ELEMENTS = 2;
@@ -24,43 +24,43 @@ namespace MyEngine
 		};
 		struct V_PosCol
 		{
-			Math::Float3 Pos{};
-			Math::Float3 Color{};
+			Float3 Pos{};
+			Float3 Color{};
 
 			static const InputLayout::Element ELEMENTS[];
 			static constexpr int NR_ELEMENTS = 2;
 		};
 		struct V_PosColNorm
 		{
-			Math::Float3 Pos{};
-			Math::Float3 Color{};
-			Math::Float3 Normal{};
+			Float3 Pos{};
+			Float3 Color{};
+			Float3 Normal{};
 
 			static const InputLayout::Element ELEMENTS[];
 			static constexpr int NR_ELEMENTS = 3;
 		};
 		struct V_PosNorCol
 		{
-			Math::Float3 Pos{};
-			Math::Float3 Normal{};
-			Math::Float3 Color{};
+			Float3 Pos{};
+			Float3 Normal{};
+			Float3 Color{};
 
 			static const InputLayout::Element ELEMENTS[];
 			static constexpr int NR_ELEMENTS = 3;
 		};
 		struct V_PosUv
 		{
-			Math::Float3 Pos{};
-			Math::Float2 Uv{};
+			Float3 Pos{};
+			Float2 Uv{};
 
 			static const InputLayout::Element ELEMENTS[];
 			static constexpr int NR_ELEMENTS = 2;
 		};
 		struct V_PosNorUv
 		{
-			Math::Float3 Pos{};
-			Math::Float3 Normal{};
-			Math::Float2 Uv{};
+			Float3 Pos{};
+			Float3 Normal{};
+			Float2 Uv{};
 
 			static const InputLayout::Element ELEMENTS[];
 			static constexpr int NR_ELEMENTS = 3;
