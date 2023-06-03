c401aed30fa8f351db71c55c8b99243251e83dab
Louis Herman <louisherman@live.be>
2023-06-03 19:07:06 +0200

    ADD(Rect): get corner functions

--- code/MyEngine/Math/Rect.cpp
+++ code/MyEngine/Math/Rect.cpp
@@ -5,3 +5,18 @@ Math::Rect::Rect() = default;
 Math::Rect::Rect(const Float2 & leftBot, const Float2 & size) : m_LeftBot{ leftBot }, m_Size{ size } {}
 Math::Rect::Rect(const Float2 & leftBot, float width, float height) : m_LeftBot{ leftBot }, m_Size{ width, height } {}
 Math::Rect::Rect(float left, float bot, const Float2 & size) : m_LeftBot{ left, bot }, m_Size{ size } {}
+
+Float2 Rect::GetLeftTop() const
+{
+	return { m_LeftBot.x, m_LeftBot.y + m_Size.y };
+}
+
+Float2 Rect::GetRightBot() const
+{
+	return { m_LeftBot.x + m_Size.x, m_LeftBot.y };
+}
+
+Float2 Rect::GetRightTop() const
+{
+	return m_LeftBot + m_Size;
+}
--- code/MyEngine/Math/Rect.h
+++ code/MyEngine/Math/Rect.h
@@ -13,6 +13,12 @@ namespace MyEngine
 			Rect(const Float2& leftBot, float width, float height);
 			Rect(float left, float bot, const Float2& size);
 
+			const Float2& GetLeftBot() const { return m_LeftBot; }
+			const Float2& GetSize() const { return m_Size; }
+			Float2 GetLeftTop() const;
+			Float2 GetRightBot() const;
+			Float2 GetRightTop() const;
+
 		private:
 			Float2 m_LeftBot;
 			Float2 m_Size;
