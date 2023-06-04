1b343b4ecf5807831172ad45b66f65e3c010e48a
Louis Herman <louisherman@live.be>
2023-06-04 23:58:39 +0200

    ADD(ScalarMath)

---
+++ code/MyEngine/Math/ScalarMath.h
@@ -0,0 +1,50 @@
+#pragma once
+
+namespace MyEngine
+{
+	namespace Math
+	{
+		template<typename T>
+		class ScalarMath
+		{
+		public:
+			template<typename D> static T Unlerp(const D& value, const D& begin, const D& end);
+			template<typename D> static D Lerp(const T& alpha, const D& begin, const D& end);
+			template<typename D> static D LerpClamp(const T& alpha, const D& begin, const D& end);
+
+			static T Clamp(const T& value, const T& min, const T& max);
+		};
+
+		template <typename T>
+		template <typename D>
+		T ScalarMath<T>::Unlerp(const D& value, const D& begin, const D& end)
+		{
+			return static_cast<T>(value - begin) / static_cast<T>(end - begin);
+		}
+
+		template <typename T>
+		template <typename D>
+		D ScalarMath<T>::Lerp(const T& alpha, const D& begin, const D& end)
+		{
+			return begin + static_cast<D>(static_cast<T>(end - begin) * alpha);
+		}
+
+		template <typename T>
+		template <typename D>
+		D ScalarMath<T>::LerpClamp(const T& alpha, const D& begin, const D& end)
+		{
+			return Clamp(Lerp(alpha, begin, end), begin, end);
+		}
+
+		template <typename T>
+		T ScalarMath<T>::Clamp(const T& value, const T& min, const T& max)
+		{
+			if (value <= min) return min;
+			if (value >= max) return max;
+			return value;
+		}
+
+		using DoubleMath = ScalarMath<double>;
+		using FloatMath = ScalarMath<float>;
+	}
+}
