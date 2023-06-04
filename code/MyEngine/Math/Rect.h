aeb1e7edffaa9535c0443064dc47c600361acdea
Louis Herman <louisherman@live.be>
2023-06-04 20:57:16 +0200

    Add(Rect): Expand & Compress functions

--- code/MyEngine/Math/Rect.h
+++ code/MyEngine/Math/Rect.h
@@ -33,6 +33,11 @@ namespace MyEngine
 			void SetWidth(const T& width) { m_Size.x = width; }
 			void SetHeight(const T& height) { m_Size.y = height; }
 
+			void Expand(const Vector4<T>& expand); //left, up, right, bot
+			Rect Expanded(const Vector4<T>& expand) const; //left, up, right, bot
+			void Compress(const Vector4<T>& compress); //left, up, right, bot
+			Rect Compressed(const Vector4<T>& compress) const; //left, up, right, bot
+
 		private:
 			Vector2<T> m_LeftBot;
 			Vector2<T> m_Size;
@@ -48,5 +53,41 @@ namespace MyEngine
 		template <typename T>T Rect<T>::GetRight() const { return m_LeftBot.x + m_Size.x; }
 		template <typename T>T Rect<T>::GetBottom() const { return m_LeftBot.y; }
 		template <typename T>T Rect<T>::GetTop() const { return m_LeftBot.y + m_Size.y; }
+
+		template <typename T>
+		void Rect<T>::Expand(const Vector4<T>& expand)
+		{
+			m_LeftBot.x -= expand.x;
+			m_LeftBot.y -= expand.w;
+			m_Size.x += expand.x + expand.z;
+			m_Size.y += expand.y + expand.w;
+		}
+
+		template <typename T>
+		Rect<T> Rect<T>::Expanded(const Vector4<T>& expand) const
+		{
+			return{
+				{m_LeftBot.x - expand.x, m_LeftBot.y - expand.w},
+				{m_Size.x + expand.x + expand.z, m_Size.y + expand.y + expand.w}
+			};
+		}
+
+		template <typename T>
+		void Rect<T>::Compress(const Vector4<T>& compress)
+		{
+			m_LeftBot.x += compress.x;
+			m_LeftBot.y += compress.w;
+			m_Size.x -= compress.x + compress.z;
+			m_Size.y -= compress.y + compress.w;
+		}
+
+		template <typename T>
+		Rect<T> Rect<T>::Compressed(const Vector4<T>& compress) const
+		{
+			return{
+				{m_LeftBot.x + compress.x, m_LeftBot.y + compress.w},
+				{m_Size.x - compress.x - compress.z, m_Size.y - compress.y - compress.w}
+			};
+		}
 	}
 }
