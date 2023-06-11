979f324c4a79d3e26645277becb7b869a43660b9
Louis Herman <louisherman@live.be>
2023-06-11 17:53:16 +0200

    ADD(PlaneGeneration): CreateVertices Pos2Uv

--- code/MyEngine/Generation/PlaneGeneration.cpp
+++ code/MyEngine/Generation/PlaneGeneration.cpp
@@ -3,6 +3,23 @@
 
 #include "Rendering/State/Mesh.h"
 
+Array<Rendering::V_Pos2Uv> Generation::PlaneGeneration::CreateVertices(const Float2& leftBottom, const Float2& size,
+	const RectFloat& uvs)
+{
+	const float left{ leftBottom.x };
+	const float bot{ leftBottom.y };
+	const float right{ left + size.x };
+	const float top{ bot + size.y };
+	Array<Rendering::V_Pos2Uv> vertices{ 6 };
+	vertices[0] = { {left,top},{uvs.GetLeft(),uvs.GetTopInv()} };
+	vertices[1] = { {right,top},{uvs.GetRight(),uvs.GetTopInv()} };
+	vertices[2] = { {right,bot},{uvs.GetRight(),uvs.GetBottom()} };
+	vertices[3] = { {left,top},{uvs.GetLeft(),uvs.GetTopInv()} };
+	vertices[4] = { {right,bot},{uvs.GetRight(),uvs.GetBottom()} };
+	vertices[5] = { {left,bot},{uvs.GetLeft(),uvs.GetBottom()} };
+	return vertices;
+}
+
 Array<Rendering::V_PosUv> Generation::PlaneGeneration::TowardsZMin(const Float3& leftBottom, const Float2& size)
 {
 	const float left{ leftBottom.x };
--- code/MyEngine/Generation/PlaneGeneration.h
+++ code/MyEngine/Generation/PlaneGeneration.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Math/RectFloat.h"
 #include "Rendering/Structs/VertexTypes.h"
 
 namespace MyEngine
@@ -12,6 +13,8 @@ namespace MyEngine
 		class PlaneGeneration
 		{
 		public:
+			static Array<Rendering::V_Pos2Uv> CreateVertices(const Float2& leftBottom, const Float2& size, const RectFloat& uvs);
+
 			static Array<Rendering::V_PosUv> TowardsZMin(const Float3& leftBottom, const Float2& size);
 			static Rendering::Mesh* MeshTowardsZMin(const Float3& leftBottom, const Float2& size);
 		};
