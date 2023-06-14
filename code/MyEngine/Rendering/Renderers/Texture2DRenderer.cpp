ad47c0311adc3635d7b6376bde250aa2218203b9
Louis Herman <louisherman@live.be>
2023-06-14 23:25:09 +0200

    ADD(Texture2DRenderer)

---
+++ code/MyEngine/Rendering/Renderers/Texture2DRenderer.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "Texture2DRenderer.h"
+
+#include "Framework/Resources.h"
+#include "Generation/PlaneGeneration.h"
+
+Rendering::Texture2DRenderer::Texture2DRenderer()
+	: m_InputLayout{ InputLayout::FromType<Instance>() }
+	, m_Shader{ Framework::Resources::GlobalShader(L"Font_Inst.hlsl") }
+{
+}
+
+void Rendering::Texture2DRenderer::Render(const Camera& camera)
+{
+	if (m_Textures.GetSize() == 0) return;
+	m_DepthStencilState.Activate();
+	m_BlendState.Activate();
+	m_RasterizerState.Activate();
+	m_SamplerState.Activate();
+
+	m_InputLayout.Activate();
+	m_Shader.Activate();
+
+	for (int i = 0; i < m_Textures.GetSize(); i++)
+	{
+		m_Textures[i].Activate();
+		m_Instances[i].Draw();
+	}
+}
+
+int Rendering::Texture2DRenderer::Add(Texture&& texture)
+{
+	const Array<Vertex> vertices{ Generation::PlaneGeneration::CreateVertices({ 0,0 }, { 1,1 }, { {0,0},{1,1} })};
+	m_Instances.Add({ vertices.GetData(), vertices.GetSizeU() });
+	m_Textures.Add(std::move(texture));
+	return m_Textures.GetSize() - 1;
+}
+
+void Rendering::Texture2DRenderer::Add(int idx, const Instance& instance)
+{
+	m_Instances[idx].Add(instance);
+}
+
+int Rendering::Texture2DRenderer::Add(Texture&& texture, const Instance& instance)
+{
+	const int idx{ Add(std::move(texture)) };
+	Add(idx, instance);
+	return idx;
+}
---
+++ code/MyEngine/Rendering/Renderers/Texture2DRenderer.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <Math\Rects.h>
+
+#include "DataStructures/List.h"
+#include "Rendering/State/BlendState.h"
+#include "Rendering/State/InputLayout.h"
+#include "Rendering/State/RasterizerState.h"
+#include "Rendering/State/SamplerState.h"
+#include "Rendering/State/Shader.h"
+#include <Rendering\State\DepthStencilState.h>
+
+#include "Rendering/DrawData/InstanceArray.h"
+#include "Rendering/DrawData/VertexList.h"
+#include "Rendering/State/Texture.h"
+#include "Rendering/Structs/Instances.h"
+#include "Rendering/Structs/VertexTypes.h"
+#include "Rendering/DrawData/InstanceArray.h"
+#include "Rendering/DrawData/InstanceList.h"
+
+namespace MyEngine
+{
+	namespace Game
+	{
+		class Camera;
+	}
+
+	namespace Rendering
+	{
+		class Texture2DRenderer
+		{
+		public:
+			//---| Types |---
+			using Instance = I_Pos2UvCol; //pos in ndc-space, for now
+
+			//---| Constructor/Destructor |---
+			Texture2DRenderer();
+			~Texture2DRenderer() = default;
+
+			//---| Functions |---
+			void Render(const Camera& camera);
+
+			int Add(Texture&& texture);
+			void Add(int idx, const Instance& instance);
+			int Add(Texture&& texture, const Instance& instance);
+
+		private:
+			using Vertex = V_Pos2Uv;
+
+			BlendState m_BlendState;
+			RasterizerState m_RasterizerState;
+			SamplerState m_SamplerState;
+			DepthStencilState m_DepthStencilState;
+
+			InputLayout m_InputLayout;
+			Shader m_Shader;
+
+			List<Texture> m_Textures;
+			List<InstanceList<Vertex, Instance>> m_Instances;
+		};
+	}
+}
