a4e0b226e9034bb1a2bbe17c201e1c7236c77400
Louis Herman <louisherman@live.be>
2023-06-15 23:59:07 +0200

    ADD(Constants): TO_DEG

--- code/MyEngine/Math/Constants.h
+++ code/MyEngine/Math/Constants.h
@@ -11,6 +11,7 @@ namespace MyEngine
 			static constexpr float PI2 = 3.14159265358979323846f * 2;
 			static constexpr float PI_DIV2 = 1.57079632679489661923f;
 			static constexpr float TO_RAD = PI / 180.f;
+			static constexpr float TO_DEG = 180.f / PI;
 			static constexpr float SQR2 = 1.4142135623730950488016887242097f;
 			static constexpr float DIVSQR2 = 1.f / SQR2;
 
