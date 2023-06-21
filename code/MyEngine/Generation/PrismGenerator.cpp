986bec6364b90abf91f1fd20179d6a71e4842338
Louis Herman <louisherman@live.be>
2023-06-21 16:13:37 +0200

    ADD(PrismGenerator)

---
+++ code/MyEngine/Generation/PrismGenerator.cpp
@@ -0,0 +1,94 @@
+#include "pch.h"
+#include "PrismGenerator.h"
+
+#include "PlaneGeneration.h"
+#include "Shapes/Quad.h"
+
+void Generation::PrismGenerator::Create(const Quad& quadBot, const Quad& quadTop, const Float3& color,
+	List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	vertices.EnsureIncrease(4 * 6);
+	indices.EnsureIncrease(6 * 6);
+	ConnectSides(quadBot, quadTop, color, vertices, indices, offset);
+	ConnectTop(quadBot, quadTop, color, vertices, indices, offset);
+	ConnectBot(quadBot, quadTop, color, vertices, indices, offset);
+}
+
+void Generation::PrismGenerator::ConnectBack(const Quad& quadBot, const Quad& quadTop, const Float3& color,
+	List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	vertices.EnsureIncrease(4);
+	indices.EnsureIncrease(6);
+	PlaneGeneration::Create(
+		quadBot.GetLeftBot() + offset, quadTop.GetLeftBot() + offset,
+		quadTop.GetRightBot() + offset, quadBot.GetRightBot() + offset, color,
+		vertices, indices);
+}
+
+void Generation::PrismGenerator::ConnectFront(const Quad& quadBot, const Quad& quadTop, const Float3& color,
+	List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	vertices.EnsureIncrease(4);
+	indices.EnsureIncrease(6);
+	PlaneGeneration::Create(
+		quadBot.GetRightTop() + offset, quadTop.GetRightTop() + offset,
+		quadTop.GetLeftTop() + offset, quadBot.GetLeftTop() + offset, color,
+		vertices, indices);
+}
+
+void Generation::PrismGenerator::ConnectLeft(const Quad& quadBot, const Quad& quadTop, const Float3& color,
+	List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	vertices.EnsureIncrease(4);
+	indices.EnsureIncrease(6);
+	PlaneGeneration::Create(
+		quadBot.GetLeftTop() + offset, quadTop.GetLeftTop() + offset,
+		quadTop.GetLeftBot() + offset, quadBot.GetLeftBot() + offset, color,
+		vertices, indices);
+}
+
+void Generation::PrismGenerator::ConnectRight(const Quad& quadBot, const Quad& quadTop, const Float3& color,
+	List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	vertices.EnsureIncrease(4);
+	indices.EnsureIncrease(6);
+	PlaneGeneration::Create(
+		quadBot.GetRightBot() + offset, quadTop.GetRightBot() + offset,
+		quadTop.GetRightTop() + offset, quadBot.GetRightTop() + offset, color,
+		vertices, indices);
+}
+
+void Generation::PrismGenerator::ConnectTop(const Quad& quadBot, const Quad& quadTop, const Float3& color,
+	List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	vertices.EnsureIncrease(4);
+	indices.EnsureIncrease(6);
+	PlaneGeneration::Create(
+		quadTop.GetLeftBot() + offset, quadTop.GetLeftTop() + offset,
+		quadTop.GetRightTop() + offset, quadTop.GetRightBot() + offset, color,
+		vertices, indices);
+}
+
+void Generation::PrismGenerator::ConnectBot(const Quad& quadBot, const Quad& quadTop, const Float3& color,
+	List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	vertices.EnsureIncrease(4);
+	indices.EnsureIncrease(6);
+	PlaneGeneration::Create(
+		quadBot.GetLeftTop() + offset, quadBot.GetLeftBot() + offset,
+		quadBot.GetRightBot() + offset, quadBot.GetRightTop() + offset, color,
+		vertices, indices);
+}
+
+void Generation::PrismGenerator::ConnectSides(
+	const Quad& quadBot, const Quad& quadTop, const Float3& color,
+	List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	constexpr unsigned nrSides = 4;
+	vertices.EnsureIncrease(4 * nrSides);
+	indices.EnsureIncrease(3 * 2 * nrSides);
+	ConnectBack(quadBot, quadTop, color, vertices, indices, offset);
+	ConnectFront(quadBot, quadTop, color, vertices, indices, offset);
+	ConnectLeft(quadBot, quadTop, color, vertices, indices, offset);
+	ConnectRight(quadBot, quadTop, color, vertices, indices, offset);
+}
---
+++ code/MyEngine/Generation/PrismGenerator.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "DataStructures/List.h"
+#include "Rendering/Structs/VertexTypes.h"
+
+namespace MyEngine
+{
+	namespace Shapes
+	{
+		struct Quad;
+	}
+
+	namespace Generation
+	{
+		class PrismGenerator
+		{
+		public:
+			static void Create(
+				const Quad& quadBot, const Quad& quadTop, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+			static void ConnectSides(
+				const Quad& quadBot, const Quad& quadTop, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+
+			static void ConnectBack(
+				const Quad& quadBot, const Quad& quadTop, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+			static void ConnectFront(
+				const Quad& quadBot, const Quad& quadTop, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+			static void ConnectLeft(
+				const Quad& quadBot, const Quad& quadTop, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+			static void ConnectRight(
+				const Quad& quadBot, const Quad& quadTop, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+			static void ConnectTop(
+				const Quad& quadBot, const Quad& quadTop, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+			static void ConnectBot(
+				const Quad& quadBot, const Quad& quadTop, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+		};
+	}
+}
