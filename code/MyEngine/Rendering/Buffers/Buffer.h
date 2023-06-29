87aa0f9042a09f53e0a0ba86a6fb755542edd0eb
Louis Herman <louisherman@live.be>
2023-06-29 18:41:05 +0200

    ADD(Buffer & InvalidateBuffer)

---
+++ code/MyEngine/Rendering/Buffers/Buffer.h
@@ -0,0 +1,120 @@
+#pragma once
+#include "Rendering/Dx/DxHelper.h"
+
+struct ID3D11Buffer;
+
+namespace MyEngine
+{
+	namespace Rendering
+	{
+		template<typename Data>
+		class Buffer
+		{
+		public:
+			//---| Construction |---
+			Buffer();
+			explicit Buffer(unsigned initCapacity, bool dynamic = true);
+			explicit Buffer(const Data* pData, unsigned count, bool dynamic = true);
+			~Buffer();
+
+			Buffer(const Buffer& other) = delete;
+			Buffer& operator=(const Buffer& other) = delete;
+
+			Buffer(Buffer&& other) noexcept;
+			Buffer& operator=(Buffer&& other) noexcept;
+
+			//---| Functions |---
+			void EnsureCapacity(unsigned minCapacity);
+			void CopyData(const Data* pData, unsigned count);
+			void ActivateVertexBuffer(unsigned slot) const;
+			unsigned GetCapacity() const { return m_Capacity; }
+
+		private:
+			ID3D11Buffer* m_pBuffer;
+			unsigned m_Capacity;
+			bool m_IsDynamic;
+		};
+
+		template <typename Data>
+		Buffer<Data>::Buffer()
+			: m_pBuffer{ nullptr }
+			, m_Capacity(0)
+			, m_IsDynamic(false)
+		{
+		}
+
+		template <typename Data>
+		Buffer<Data>::Buffer(unsigned initCapacity, bool dynamic)
+			: m_pBuffer{ nullptr }
+			, m_Capacity{ initCapacity }
+			, m_IsDynamic{ dynamic }
+		{
+			Dx::DxHelper::CreateVertexBuffer(m_pBuffer, sizeof(Data), m_Capacity, !m_IsDynamic);
+		}
+
+		template <typename Data>
+		Buffer<Data>::Buffer(const Data* pData, unsigned count, bool dynamic)
+			: m_pBuffer{ nullptr }
+			, m_Capacity{ count }
+			, m_IsDynamic{ dynamic }
+		{
+			Dx::DxHelper::CreateVertexBuffer(m_pBuffer, pData, static_cast<int>(count), !dynamic);
+		}
+
+		template <typename Data>
+		Buffer<Data>::~Buffer()
+		{
+			if (m_pBuffer)
+			{
+				m_pBuffer->Release();
+				m_pBuffer = nullptr;
+			}
+			m_Capacity = 0;
+		}
+
+		template <typename Data>
+		Buffer<Data>::Buffer(Buffer&& other) noexcept
+			: m_pBuffer{ other.m_pBuffer }
+			, m_Capacity{ other.m_Capacity }
+			, m_IsDynamic{ other.m_IsDynamic }
+		{
+			other.m_pBuffer = nullptr;
+			other.m_Capacity = 0;
+		}
+
+		template <typename Data>
+		Buffer<Data>& Buffer<Data>::operator=(Buffer&& other) noexcept
+		{
+			if (&other == this) return *this;
+			if (m_pBuffer) m_pBuffer->Release();
+			m_pBuffer = other.m_pBuffer;
+			m_Capacity = other.m_Capacity;
+			m_IsDynamic = other.m_IsDynamic;
+			other.m_pBuffer = nullptr;
+			other.m_Capacity = 0;
+			return*this;
+		}
+
+		template <typename Data>
+		void Buffer<Data>::EnsureCapacity(unsigned minCapacity)
+		{
+			if (minCapacity <= m_Capacity) return;
+			m_pBuffer->Release();
+			Dx::DxHelper::CreateVertexBuffer(m_pBuffer, sizeof(Data), minCapacity, !m_IsDynamic);
+		}
+
+		template <typename Data>
+		void Buffer<Data>::CopyData(const Data* pData, unsigned count)
+		{
+			Dx::DxHelper::UpdateBuffer(m_pBuffer, pData, count);
+		}
+
+		template <typename Data>
+		void Buffer<Data>::ActivateVertexBuffer(unsigned slot) const
+		{
+			const unsigned stride{ sizeof(Data) };
+			constexpr unsigned offset{ 0 }; //todo: required?
+			Globals::pGpu->GetContext().IASetVertexBuffers(slot, 1, &m_pBuffer, &stride, &offset);
+		}
+	}
+}
---
+++ code/MyEngine/Rendering/Buffers/InvalidateBuffer.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "Buffer.h"
+#include "DataStructures/InvalidateList.h"
+
+namespace MyEngine
+{
+	namespace Rendering
+	{
+		template<typename Data>
+		class InvalidateBuffer
+		{
+		public:
+			//---| Construction |---
+			InvalidateBuffer() = default;
+			explicit InvalidateBuffer(unsigned initCapacity);
+
+			//---| Functions |---
+			int Add(Data&& data);
+			Data Remove(int idx);
+
+			const InvalidateList<Data>& GetCpuData() const { return m_CpuBuffer; }
+			InvalidateList<Data>& GetCpuData() { return m_CpuBuffer; }
+
+			unsigned GetCount() const { return m_CpuBuffer.GetSize(); }
+			const Data* GetFirst() const { return m_CpuBuffer.GetFirst(); }
+			const Data* GetLast() const { return m_CpuBuffer.GetLast(); }
+			const Data* GetEnd() const { return m_CpuBuffer.GetEnd(); }
+			Data* GetFirst() { return m_CpuBuffer.GetFirst(); }
+			Data* GetLast() { return m_CpuBuffer.GetLast(); }
+			Data* GetEnd() { return m_CpuBuffer.GetEnd(); }
+
+			const Data& operator[](int idx) const { return m_CpuBuffer.GetData()[idx]; }
+			Data& operator[](int idx) { return m_CpuBuffer.GetData()[idx]; }
+
+			void ActivateVertexBuffer(unsigned slot);
+			void Draw(unsigned nrVertices) const;
+
+		private:
+			Buffer<Data> m_GpuBuffer;
+			InvalidateList<Data> m_CpuBuffer;
+		};
+
+		template <typename Data>
+		InvalidateBuffer<Data>::InvalidateBuffer(unsigned initCapacity)
+			: m_GpuBuffer{ initCapacity, true }
+			, m_CpuBuffer{ initCapacity }
+		{
+		}
+
+		template <typename Data>
+		int InvalidateBuffer<Data>::Add(Data&& data)
+		{
+			return m_CpuBuffer.Add(std::move(data));
+		}
+
+		template <typename Data>
+		Data InvalidateBuffer<Data>::Remove(int idx)
+		{
+			return m_CpuBuffer.Remove(idx);
+		}
+
+		template <typename Data>
+		void InvalidateBuffer<Data>::ActivateVertexBuffer(unsigned slot)
+		{
+			if (m_CpuBuffer.HasChanged())
+			{
+				m_CpuBuffer.ClearChangedFlag();
+				m_GpuBuffer.EnsureCapacity(m_CpuBuffer.GetCapacity());
+				m_GpuBuffer.CopyData(m_CpuBuffer.GetFirst(), m_CpuBuffer.GetSize());
+			}
+			m_GpuBuffer.ActivateVertexBuffer(slot);
+		}
+
+		template <typename Data>
+		void InvalidateBuffer<Data>::Draw(unsigned nrVertices) const
+		{
+			Globals::pGpu->GetContext().DrawInstanced(nrVertices, GetCount(), 0, 0);
+		}
+	}
+}
