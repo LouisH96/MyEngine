a29562044b11baea97beb7a2e83a0196632648ba
Louis Herman <louisherman@live.be>
2023-07-02 01:58:14 +0200

    ADD(FontRenderer): use entries to store entry characters

--- code/MyEngine/Gui/FontRenderer.cpp
+++ code/MyEngine/Gui/FontRenderer.cpp
@@ -18,9 +18,6 @@ Gui::FontRenderer::FontRenderer()
 
 	m_UvXHeight = m_CharLookup.GetCharUvHeight('x');
 	m_UvHeightToWidth = Float::Cast(fontAtlas.GetImage().GetHeight()) / Float::Cast(fontAtlas.GetImage().GetWidth());
-
-	Add('x', { 0,-1 }, { 0,50 }, 75, { 1,0,0 });
-	Add("My Engine", { 0,-1 }, { 0,50 + 75 }, 50, { 0,1,0 });
 }
 
 void Gui::FontRenderer::OnCanvasResized(const App::ResizedEvent& event)
@@ -40,22 +37,17 @@ void Gui::FontRenderer::Render()
 
 void Gui::FontRenderer::Remove(int id)
 {
-	m_Renderer.Remove(id);
+	m_Entries.Invalidate(id);
+	const Entry& entry{ m_Entries.GetData()[id] };
+
+	for (int i = 0; i < entry.Characters.GetSize(); i++)
+		m_Renderer.Remove(entry.Characters[i]);
 }
 
-int Gui::FontRenderer::Add(char c, const Float2& pivot, const Float2& offset, float height, const Float3& color)
+int Gui::FontRenderer::Add(char c, const Float2& pivot, const Float2& offset, const Float3& color,
+	const Float2& screenSize, const RectFloat& uvRect)
 {
-	Float2 screenSize{};
-	return Add(c, pivot, offset, height, color, screenSize);
-}
-
-int Gui::FontRenderer::Add(char c, const Float2& pivot, const Float2& offset, float height, const Float3& color,
-	Float2& outScreenSize)
-{
-	RectFloat uvRect{};
-	m_CharLookup.Lookup(c, height, uvRect, outScreenSize);
-
-	const int idx{ m_Renderer.Add(pivot, offset, outScreenSize) };
+	const int idx{ m_Renderer.Add(pivot, offset, screenSize) };
 	Instance& instance{ m_Renderer.Get(idx) };
 	instance.color = color;
 	instance.uvOffset = uvRect.GetLeftBot();
@@ -63,12 +55,21 @@ int Gui::FontRenderer::Add(char c, const Float2& pivot, const Float2& offset, fl
 	return idx;
 }
 
-void Gui::FontRenderer::Add(const std::string& text, const Float2& pivot, const Float2& offset, float height,
+int Gui::FontRenderer::Add(const std::string& text, const Float2& pivot, const Float2& offset, float height,
 	const Float3& color, float spacing)
 {
-	const float totalWidth{ m_CharLookup.GetScreenWidth(text, height) };
+	//retrieve existing entry struct (so an existing list can be reused)
+	Entry* pEntry;
+	const int idx{ m_Entries.Validate(pEntry) };
+	pEntry->Active = true;
+	pEntry->Characters.Clear();
 
-	float currentX{ offset.x - totalWidth / 2 };
+	//calculate the total text size (used for alignment)
+	const Float2 totalSize{ m_CharLookup.GetScreenSize(text, height) };
+
+	//add each character
+	const Float2 normalizedPivot{ (pivot + Float2{1,1}) / 2 };
+	float currentX{ offset.x - totalSize.x * normalizedPivot.x };
 	for (const char c : text)
 	{
 		if (c == ' ')
@@ -76,9 +77,70 @@ void Gui::FontRenderer::Add(const std::string& text, const Float2& pivot, const 
 			currentX += m_CharLookup.GetScreenSpaceWidth(height);
 			continue;
 		}
-		const float halfWidth{ m_CharLookup.GetScreenWidth(c, height) / 2 };
-		currentX += halfWidth;
-		Add(c, pivot, { currentX, offset.y }, height, color);
-		currentX += halfWidth + spacing;
+		//calculate & add characters rect
+		RectFloat charUvRect;
+		Float2 charScreenSize;
+		m_CharLookup.Lookup(c, height, charUvRect, charScreenSize);
+
+		const float charYOffset{ (charScreenSize.y - totalSize.y) * normalizedPivot.y };
+
+		currentX += charScreenSize.x * normalizedPivot.x;
+		pEntry->Characters.Add(Add(c, pivot, { currentX, offset.y + charYOffset }, color, charScreenSize, charUvRect));
+		currentX += charScreenSize.x * (1 - normalizedPivot.x);
 	}
+	return idx;
+}
+
+int Gui::FontRenderer::AddLeftTop(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { -1,1 }, offset, height, color, spacing);
+}
+
+int Gui::FontRenderer::AddLeftCenter(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { -1, 0 }, offset, height, color, spacing);
+}
+
+int Gui::FontRenderer::AddLeftBot(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { -1,-1 }, offset, height, color, spacing);
+}
+
+int Gui::FontRenderer::AddCenterTop(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { 0, 1 }, offset, height, color, spacing);
+}
+
+int Gui::FontRenderer::AddCenter(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { 0,0 }, offset, height, color, spacing);
+}
+
+int Gui::FontRenderer::AddCenterBot(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { 0, -1 }, offset, height, color, spacing);
+}
+
+int Gui::FontRenderer::AddRightTop(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { 1, 1 }, offset, height, color, spacing);
+}
+
+int Gui::FontRenderer::AddRightCenter(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { 1, 0 }, offset, height, color, spacing);
+}
+
+int Gui::FontRenderer::AddRightBot(const std::string& text, float height, const Float2& offset, const Float3& color,
+	float spacing)
+{
+	return Add(text, { 1, -1 }, offset, height, color, spacing);
 }
--- code/MyEngine/Gui/FontRenderer.h
+++ code/MyEngine/Gui/FontRenderer.h
@@ -20,14 +20,32 @@ namespace MyEngine
 			void Render();
 
 			void Remove(int id);
-			int Add(char c, const Float2& pivot, const Float2& offset, float height, const Float3& color);
-			int Add(char c, const Float2& pivot, const Float2& offset, float height, const Float3& color, Float2& outScreenSize);
+			int Add(const std::string& text, const Float2& pivot, const Float2& offset, float height, const Float3& color, float spacing = 0);
 
-			void Add(const std::string& text, const Float2& pivot, const Float2& offset, float height, const Float3& color, float spacing = 0);
+			int AddLeftTop(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+			int AddLeftCenter(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+			int AddLeftBot(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+
+			int AddCenterTop(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+			int AddCenter(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+			int AddCenterBot(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+
+			int AddRightTop(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+			int AddRightCenter(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+			int AddRightBot(const std::string& text, float height, const Float2& offset, const Float3& color, float spacing = 0);
+
 
 		private:
 			using Vertex = Rendering::V_Pos2Uv;
 			using Instance = Rendering::I_Pos2UvCol;
+			struct Entry
+			{
+				bool Active{ false };
+				List<int> Characters{};
+
+				static bool IsValid(const Entry& entry) { return entry.Active; }
+				static void Invalidate(Entry& entry) { entry.Active = false; }
+			};
 
 			Rendering::NdcRectRenderer<Vertex, Instance> m_Renderer;
 			Rendering::Texture m_FontAtlas;
@@ -38,6 +56,10 @@ namespace MyEngine
 			Rendering::FontAtlasLookup m_CharLookup;
 			float m_UvXHeight;
 			float m_UvHeightToWidth;
+
+			InvalidateList<Entry> m_Entries;
+
+			int Add(char c, const Float2& pivot, const Float2& offset, const Float3& color, const Float2& screenSize, const RectFloat& uvRect);
 		};
 	}
 }
