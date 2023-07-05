9e2a65bb88ed59b7dbcb7ebc15913d5d37590fb5
Louis Herman <louisherman@live.be>
2023-07-05 20:50:52 +0200

    ADD(IdxBuffer)

---
+++ code/MyEngine/Rendering/Buffers/IdxBuffer.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "IdxBuffer.h"
+
+Rendering::IdxBuffer::IdxBuffer()
+	: m_pBuffer{ nullptr }
+	, m_Capacity{ 0 }
+{
+}
+
+Rendering::IdxBuffer::IdxBuffer(const int* pData, unsigned count)
+	: m_pBuffer{ nullptr }
+	, m_Capacity{ count }
+{
+	Dx::DxHelper::CreateIndexBuffer(m_pBuffer, pData, static_cast<int>(count));
+}
+
+Rendering::IdxBuffer::~IdxBuffer()
+{
+	if (m_pBuffer)
+	{
+		m_pBuffer->Release();
+		m_pBuffer = nullptr;
+		m_Capacity = 0;
+	}
+}
+
+Rendering::IdxBuffer::IdxBuffer(IdxBuffer&& other) noexcept
+	: m_pBuffer{ other.m_pBuffer }
+	, m_Capacity{ other.m_Capacity }
+{
+	other.m_pBuffer = nullptr;
+	other.m_Capacity = 0;
+}
+
+Rendering::IdxBuffer& Rendering::IdxBuffer::operator=(IdxBuffer&& other) noexcept
+{
+	if (&other == this) return *this;
+	if (m_pBuffer) m_pBuffer->Release();
+	m_pBuffer = other.m_pBuffer;
+	m_Capacity = other.m_Capacity;
+	other.m_pBuffer = nullptr;
+	other.m_Capacity = 0;
+	return *this;
+}
+
+void Rendering::IdxBuffer::Activate() const
+{
+	Globals::pGpu->GetContext().IASetIndexBuffer(m_pBuffer, DXGI_FORMAT_R32_UINT, 0);
+}
---
+++ code/MyEngine/Rendering/Buffers/IdxBuffer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+struct ID3D11Buffer;
+
+namespace MyEngine
+{
+	namespace Rendering
+	{
+		class IdxBuffer
+		{
+		public:
+			//---| Construction |---
+			IdxBuffer();
+			explicit IdxBuffer(const int* pData, unsigned count);
+			~IdxBuffer();
+
+			IdxBuffer(const IdxBuffer& other) = delete;
+			IdxBuffer& operator=(const IdxBuffer& other) = delete;
+			IdxBuffer(IdxBuffer&& other) noexcept;
+			IdxBuffer& operator=(IdxBuffer&& other) noexcept;
+
+			//---| Functions |---
+			void Activate() const;
+			unsigned GetCapacity() const { return m_Capacity; }
+
+		private:
+			ID3D11Buffer* m_pBuffer;
+			unsigned m_Capacity;
+		};
+	}
+}
