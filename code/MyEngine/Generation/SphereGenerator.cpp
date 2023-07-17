f8b9d6d7ff50e4f83a47ee9b52b2077c0554455c
Louis Herman <louisherman@live.be>
2023-07-17 17:47:25 +0200

    ADD(SphereGenerator)

---
+++ code/MyEngine/Generation/SphereGenerator.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "SphereGenerator.h"
+
+#include "Shapes.h"
+
+unsigned Generation::SphereGenerator::GetNrVertices(unsigned nrCols, unsigned nrRows)
+{
+	const int nrMiddlePointRows = nrRows - 1;
+	const int nrMiddlePoints = nrMiddlePointRows * nrCols;
+	const int nrPoints = nrMiddlePoints + 2;
+	return nrPoints;
+}
+
+void Generation::SphereGenerator::Generate(const Sphere& sphere, unsigned nrCols, unsigned nrRows, const Float3& color,
+                                           List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset)
+{
+	Array<Float3> positions{};
+	Array<Float3> normals{};
+	Array<int> sphereIndices{};
+	Shapes::GenerateSphereBuffers(sphere, nrCols, nrRows, positions, normals, sphereIndices);
+
+	indices.EnsureIncrease(sphereIndices.GetSizeU());
+	for (unsigned i = 0, begin = vertices.GetSizeU(); i < sphereIndices.GetSizeU(); i++)
+		indices.Add(sphereIndices[i] + begin);
+
+	vertices.EnsureIncrease(positions.GetSizeU());
+	for (unsigned i = 0; i < positions.GetSizeU(); i++)
+		vertices.Add({ positions[i], color, normals[i] });
+}
---
+++ code/MyEngine/Generation/SphereGenerator.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace MyEngine
+{
+	class Sphere;
+}
+
+namespace MyEngine
+{
+	namespace Generation
+	{
+		class SphereGenerator
+		{
+		public:
+			static unsigned GetNrVertices(unsigned nrCols, unsigned nrRows);
+			static void Generate(const Sphere& sphere, unsigned nrCols, unsigned nrRows, const Float3& color,
+				List<Rendering::V_PosColNorm>& vertices, List<int>& indices, const Float3& offset = {});
+		};
+	}
+}
