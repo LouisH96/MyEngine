967ca039a19849281885ba75441f4370c501e15b
Louis Herman <louisherman@live.be>
2023-07-22 16:52:59 +0200

    ADD(LineSphereCollision): with templated PtrRange

--- code/MyEngine/Physics/Collisions/LineSphereCollision.h
+++ code/MyEngine/Physics/Collisions/LineSphereCollision.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Geometry/Shapes/Sphere.h"
 
 namespace MyEngine
 {
@@ -17,6 +18,37 @@ namespace MyEngine
 			static int Detect(
 				const Ray& ray,
 				PtrRangeConst<Sphere> spheres);
+
+			template<typename PtrRange>
+			static int Detect(
+				const Ray& ray,
+				PtrRange spheres);
 		};
+
+		template <typename PtrRange>
+		int LineSphereCollision::Detect(const Ray& ray, PtrRange spheres)
+		{
+			int idx{ -1 };
+			float closest{ Float::Max() };
+			for (int i = 0; i < spheres.count; i++)
+			{
+				const Sphere& sphere{ spheres[i] };
+				const Float3 toCenter{ sphere.GetCenter() - ray.Origin };
+				const float dot{ ray.Direction.Dot(toCenter) };
+				if (dot < 0 || dot > ray.Length) continue;
+				const Float3 closestPoint{ ray.GetPoint(dot) };
+				const float distanceSq{ closestPoint.DistanceSq(sphere.GetCenter()) };
+				if (distanceSq > sphere.GetRadiusSq()) continue;
+				const float distance{ sqrtf(distanceSq) };
+				const float angleSin{ distance / sphere.GetRadius() };
+				const float angleCos{ cosf(asinf(angleSin)) };
+				const float distanceInside{ angleCos * sphere.GetRadius() };
+				const float t{ dot - distanceInside };
+				if (t >= closest) continue;
+				idx = i;
+				closest = t;
+			}
+			return idx;
+		}
 	}
 }
