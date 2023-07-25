9916c86ce546df97dad7bfdad47e58e8f4857465
Louis Herman <louisherman@live.be>
2023-07-25 21:29:17 +0200

    ADD(Cone): Direction & helper functions

--- code/MyEngine/Geometry/Shapes/Cone.cpp
+++ code/MyEngine/Geometry/Shapes/Cone.cpp
@@ -1,9 +1,32 @@
 #include "pch.h"
 #include "Cone.h"
 
-Geometry::Cone::Cone(const Float3& origin, float length, float radius)
+Cone::Cone(const Float3& origin, const Float3& direction, float length, float radius)
 	: origin{ origin }
+	, direction{ direction }
 	, length{ length }
 	, radius{ radius }
 {
+}
+
+Float3 Cone::GetEnd() const
+{
+	return origin + direction * length;
+}
+
+Float3 Cone::GetRight() const
+{
+	if (direction == Float3{0, 1, 0})
+		return { 1,0,0 };
+	return Float3{ 0,1,0 }.Cross(direction).NormalizedSafe();
+}
+
+Float3 Cone::GetUp(const Float3& right) const
+{
+	return direction.Cross(right).Normalized();
+}
+
+float Cone::GetAngle() const
+{
+	return atan2f(radius, length);
 }
--- code/MyEngine/Geometry/Shapes/Cone.h
+++ code/MyEngine/Geometry/Shapes/Cone.h
@@ -2,17 +2,20 @@
 
 namespace MyEngine
 {
-	namespace Geometry
+	class Cone
 	{
-		class Cone
-		{
-		public:
-			Cone() = default;
-			Cone(const Float3& origin, float length, float radius);
+	public:
+		Cone() = default;
+		Cone(const Float3& origin, const Float3& direction, float length, float radius);
 
-			Float3 origin;
-			float length;
-			float radius;
-		};
-	}
+		Float3 origin;
+		Float3 direction;
+		float length;
+		float radius;
+
+		Float3 GetEnd() const;
+		Float3 GetRight() const;
+		Float3 GetUp(const Float3& right) const;
+		float GetAngle() const;
+	};
 }
