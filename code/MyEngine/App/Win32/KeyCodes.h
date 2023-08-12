5f32f13d49ba4b3cd42efc8f64495c6aa9215125
Louis Herman <louisherman@live.be>
2023-08-12 06:24:24 +0200

    ADD(KeyCodes): Space

--- code/MyEngine/App/Win32/KeyCodes.h
+++ code/MyEngine/App/Win32/KeyCodes.h
@@ -1,10 +1,12 @@
 #pragma once
 
+
 namespace MyEngine
 {
 	class KeyCodes
 	{
 	public:
+		static constexpr char SPACE = 0x20;
 		static constexpr char ESCAPE = '\x1b';
 		static constexpr char SHIFT_LEFT = '\x10';
 		static constexpr char CTRL_LEFT = '\x11';
