6153718ef83a45cb11f156453f91893497e1c194
Louis Herman <louisherman@live.be>
2023-09-25 16:00:36 +0200

    ADD(ArrayAdder): Add(V,V,V)

--- code/MyBasics/DataStructures/Adders/ArrayAdder.h
+++ code/MyBasics/DataStructures/Adders/ArrayAdder.h
@@ -10,6 +10,7 @@ namespace MyEngine
 		ArrayAdder(Array<T>& array, unsigned startIdx);
 
 		void Add(const T& value);
+		void Add(const T& value1, const T& value2, const T& value3);
 
 	private:
 		T* m_pCurrent;
@@ -26,5 +27,13 @@ namespace MyEngine
 	void ArrayAdder<T>::Add(const T& value)
 	{
 		*m_pCurrent++ = value;
+	}
+
+	template <typename T>
+	void ArrayAdder<T>::Add(const T& value1, const T& value2, const T& value3)
+	{
+		*m_pCurrent++ = value1;
+		*m_pCurrent++ = value2;
+		*m_pCurrent++ = value3;
 	}
 }
