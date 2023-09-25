8fb6a23a8e09685a149746a38ee99a2274fc8aaf
Louis Herman <louisherman@live.be>
2023-09-25 16:01:11 +0200

    ADD(ListAdder)

---
+++ code/MyBasics/DataStructures/Adders/ListAdder.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "DataStructures/List.h"
+
+namespace MyEngine
+{
+	template<typename D>
+	class ListAdder
+	{
+	public:
+		explicit ListAdder(List<D>& list);
+
+		void Add(const D& value);
+		void Add(const D& value1, const D& value2, const D& value3);
+
+	private:
+		List<D>* m_pList;
+	};
+
+	template <typename D>
+	ListAdder<D>::ListAdder(List<D>& list)
+		: m_pList{ &list }
+	{
+	}
+
+	template <typename D>
+	void ListAdder<D>::Add(const D& value)
+	{
+		m_pList->Add(value);
+	}
+
+	template <typename D>
+	void ListAdder<D>::Add(const D& value1, const D& value2, const D& value3)
+	{
+		m_pList->Add(value1, value2, value3);
+	}
+}
