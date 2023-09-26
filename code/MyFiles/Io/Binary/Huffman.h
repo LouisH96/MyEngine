f2e7468d2576254405cfaadf82a51979a4caab0a
Louis Herman <louisherman@live.be>
2023-09-26 17:04:20 +0200

    WARNING(Huffman)

--- code/MyFiles/Io/Binary/Huffman.h
+++ code/MyFiles/Io/Binary/Huffman.h
@@ -22,29 +22,29 @@ namespace MyEngine
 				//step 1
 				const uint8_t highestNrOfCl{ Algorithms::GetBiggest(codeLengths) };
 				Array<Code> clCount{ highestNrOfCl + 1, 0 };
-				for (int i = 0; i < codeLengths.GetSize(); i++)
+				for (unsigned i = 0; i < codeLengths.GetSize(); i++)
 					if (codeLengths[i] > 0)
-						clCount[codeLengths[i]]++;
+						++clCount[codeLengths[i]];
 
 				//step 2
 				Array<Code> codeOffsets{ highestNrOfCl+1, 0 };
 				Code code = 0;
 				uint8_t prevCount = 0;
-				for (int i = 1; i < codeOffsets.GetSize(); i++)
+				for (unsigned i = 1; i < codeOffsets.GetSize(); i++)
 					//if (clCount[i] > 0)
 					{
-						code = (code + prevCount) << 1;
+						code = static_cast<Code>((code + static_cast<unsigned>(prevCount)) << 1u);
 						codeOffsets[i] = code;
-						prevCount = clCount[i];
+						prevCount = static_cast<uint8_t>(clCount[i]);
 					}
 
 				//step 3
 				Array<Code> codes{ codeLengths.GetSize(), 0 };
-				for (int i = 0; i < codes.GetSize(); i++)
+				for (unsigned i = 0; i < codes.GetSize(); i++)
 					if (codeLengths[i] > 0)
 					{
 						codes[i] = codeOffsets[codeLengths[i]];
-						codeOffsets[codeLengths[i]]++;
+						++codeOffsets[codeLengths[i]];
 					}
 
 				return codes;
