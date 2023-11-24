0dcef71130c86851ceee94f6384ba4fe9de62454
Louis Herman <louisherman@live.be>
2023-11-24 16:17:13 +0100

    ADD(Convert): ToString w. template

--- code/MyBasics/String/Convert.h
+++ code/MyBasics/String/Convert.h
@@ -7,7 +7,17 @@ namespace MyEngine
 	{
 	public:
 		static char ToChar(unsigned oneNumber);
+
 		static std::string ToString(const std::wstring& wString);
 		static std::string ToString(char c);
+
+		template<typename T>
+		static std::string ToString(T value);
 	};
+
+	template <typename T>
+	std::string Convert::ToString(T value)
+	{
+		return std::to_string(value);
+	}
 }
