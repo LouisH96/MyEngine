887868e150f66017505e09729fedae91b091ca05
Louis Herman <louisherman@live.be>
2024-02-08 21:18:30 +0100

    ADD(NewUi::Margin)

---
+++ code/MyEngine/Applied/NewUi/Elements/Margin.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include "Margin.h"
+
+using namespace NewUi;
+
+Margin::Margin(float margin)
+	: m_Margin{ margin }
+{
+}
+
+void Margin::UpdateSizeAndTreePositions(const ResizePref& pref)
+{
+	ResizePref childPref;
+	childPref.horMode = pref.horMode;
+	childPref.verMode = pref.verMode;
+	childPref.minSize = {};
+	childPref.maxSize = pref.maxSize - m_Margin * 2;
+
+	Float2 childBounds{};
+	for (unsigned i = 0; i < GetNrChildren(); i++)
+	{
+		UpdateChildSize(i, childPref);
+		SetChildPosition(i, Float2{ m_Margin });
+
+		childBounds.x = Float::Max(childBounds.x, GetChild(i).GetWidth());
+		childBounds.y = Float::Max(childBounds.y, GetChild(i).GetHeight());
+	}
+
+	SetSize(childBounds + m_Margin * 2);
+}
+
+void Margin::Clear()
+{
+}
+
+void Margin::Create()
+{
+}
