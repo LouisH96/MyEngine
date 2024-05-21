dc8e73637163655ba365eccf5cc36c923a3afa76
Louis Herman <louisherman@live.be>
2024-05-21 07:05:45 +0200

    ADD(MeshListData): Setup
    5

--- code/MyEngine/Rendering/Mesh/MeshListData.h
+++ code/MyEngine/Rendering/Mesh/MeshListData.h
@@ -46,4 +46,41 @@ namespace MyEngine
 		return ListAdder<int>(Indices);
 	}
 
+	//---| Extra |---
+	class TestAllTopologies
+	{
+	public:
+		template<typename Vertex, typename Generator, typename Combinator, typename Options>
+		static void TestAll(Combinator combinator, const Options& options, std::string testName);
+
+		template<typename Vertex, typename Generator, typename Combinator, typename Options, ModelTopology Topology>
+		static void Test(Combinator combinator, const Options& options);
+	};
+	template<typename Vertex, typename Generator, typename Combinator, typename Options>
+	inline void TestAllTopologies::TestAll(Combinator combinator, const Options& options, std::string testName)
+	{
+		std::cout << "Test all topologies for " << testName << std::endl;
+		//Test<Vertex, Generator, Combinator, Options, ModelTopology::TriangleList>(combinator, options);
+		Test<Vertex, Generator, Combinator, Options, ModelTopology::TriangleListIdx>(combinator, options);
+		//Test<Vertex, Generator, Combinator, Options, ModelTopology::TriangleStrip>(combinator, options);
+		//Test<Vertex, Generator, Combinator, Options, ModelTopology::TriangleStripIdx>(combinator, options);
+	}
+
+	template<typename Vertex, typename Generator, typename Combinator, typename Options, ModelTopology Topology>
+	inline void TestAllTopologies::Test(Combinator combinator, const Options& options)
+	{
+		MeshListData<Vertex, Topology> data{};
+		Generator::Generate(combinator, data, 0, options);
+
+		const unsigned nrVertices{ data.Vertices.GetSize() };
+		const unsigned nrIndices{ data.Indices.GetSize() };
+		const unsigned memory{ nrVertices * sizeof(Vertex) + nrIndices * sizeof(int) };
+
+		std::cout << "---| "
+			<< ModelTopologyToString::ToString<Topology>()
+			<< " |---\n"
+			<< "\t Vertices: " << nrVertices << "\n"
+			<< "\t Indices: " << nrIndices << "\n"
+			<< "\t Memory: " << memory << " byte\n";
+	}
 }
