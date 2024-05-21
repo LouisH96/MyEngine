fce46699fbcb476f6c0da2d3d8da5c5a8e7594f6
Louis Herman <louisherman@live.be>
2024-05-21 07:01:45 +0200

    ADD(ModelTopology): ToString

--- code/MyEngine/Geometry/ModelTopology.h
+++ code/MyEngine/Geometry/ModelTopology.h
@@ -11,4 +11,52 @@ namespace MyEngine
 
 		Unknown
 	};
+
+	class ModelTopologyToString
+	{
+	public:
+		template<ModelTopology Topology>
+		static std::string ToString();
+	};
+
+	template<>
+	inline std::string ModelTopologyToString::ToString<ModelTopology::LineList>()
+	{
+		return "LineList";
+	}
+	template<>
+	inline std::string ModelTopologyToString::ToString<ModelTopology::LineListIdx>()
+	{
+		return "LineListIdx";
+	}
+	template<>
+	inline std::string ModelTopologyToString::ToString<ModelTopology::LineStrip>()
+	{
+		return "LineStrip";
+	}
+	template<>
+	inline std::string ModelTopologyToString::ToString<ModelTopology::LineStripIdx>()
+	{
+		return "LineStripIdx";
+	}
+	template<>
+	inline std::string ModelTopologyToString::ToString<ModelTopology::TriangleList>()
+	{
+		return "TriangleList";
+	}
+	template<>
+	inline std::string ModelTopologyToString::ToString<ModelTopology::TriangleListIdx>()
+	{
+		return "TriangleListIdx";
+	}
+	template<>
+	inline std::string ModelTopologyToString::ToString<ModelTopology::TriangleStrip>()
+	{
+		return "TriangleStrip";
+	}
+	template<>
+	inline std::string ModelTopologyToString::ToString<ModelTopology::TriangleStripIdx>()
+	{
+		return "TriangleStripIdx";
+	}
 }
