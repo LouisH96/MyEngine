3b20e2663518d20b54c3bd2a64beaac48baf33fd
Louis Herman <louisherman@live.be>
2024-08-05 03:40:05 +0200

    ADD(HoleArray): helper functions

--- source/MyEngine/Applied/MeshMaker/Shapes/HoleArray.h
+++ source/MyEngine/Applied/MeshMaker/Shapes/HoleArray.h
@@ -7,8 +7,8 @@ namespace MeshMaker
 class HoleArray
 {
 public:
-	HoleArray(unsigned nrHoles, 
-		float holeRadius, float holeGap, unsigned nrCornersPerHole );
+	HoleArray(unsigned nrHoles,
+		float holeRadius, float holeGap, unsigned nrCornersPerHole);
 
 	float GetHoleRadius() const { return m_HoleRadius; }
 	float GetHoleGap() const { return m_HoleGap; }
@@ -16,6 +16,9 @@ public:
 	unsigned GetNrCornersPerHole() const { return m_NrCornersPerHole; }
 
 	unsigned GetNrGaps() const { return m_NrHoles - 1; }
+
+	float GetArrayLength() const { return (m_HoleRadius * 2 + m_HoleGap) * m_NrHoles - m_HoleGap; };
+	float GetArrayWidth() const { return m_HoleRadius * 2; }
 
 private:
 	unsigned m_NrHoles;
