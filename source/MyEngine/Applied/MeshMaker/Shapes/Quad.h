759e6c64ee4b9e35bec08fa3e40715849b201213
Louis Herman <louisherman@live.be>
2024-08-05 03:39:49 +0200

    ADD(Quad): helper functions

--- source/MyEngine/Applied/MeshMaker/Shapes/Quad.h
+++ source/MyEngine/Applied/MeshMaker/Shapes/Quad.h
@@ -18,6 +18,16 @@ public:
 		: Vertices{ p0, p1, p2, p3 }
 	{};
 
+	SharedPtr<const MakerVertex> GetLeftBot() const { return Vertices[0]; }
+	SharedPtr<const MakerVertex> GetLeftTop() const { return Vertices[1]; }
+	SharedPtr<const MakerVertex> GetRightBot() const { return Vertices[3]; }
+	SharedPtr<const MakerVertex> GetRightTop() const { return Vertices[2]; }
+
+	void SetLeftBot(SharedPtr<const MakerVertex> vertex) { Vertices[0] = vertex; }
+	void SetLeftTop(SharedPtr<const MakerVertex> vertex) { Vertices[1] = vertex; }
+	void SetRightBot(SharedPtr<const MakerVertex> vertex) { Vertices[3] = vertex; }
+	void SetRightTop(SharedPtr<const MakerVertex> vertex) { Vertices[2] = vertex; }
+
 	SharedPtr<const MakerVertex> Vertices[NrVertices];
 };
 }
