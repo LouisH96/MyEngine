998632e47ca285cea901315728fa0e719c6cfd5e
Louis Herman <louisherman@live.be>
2024-09-14 20:45:48 +0200

    ADD(Constants): PI_DIV2_S

--- source/MyBasics/Math/Constants.h
+++ source/MyBasics/Math/Constants.h
@@ -9,6 +9,7 @@ namespace MyEngine
 		static constexpr float PI = 3.14159265358979323846f;
 		static constexpr float PI2 = 3.14159265358979323846f * 2;
 		static constexpr float PI_DIV2 = 1.57079632679489661923f;
+		static constexpr float PI_DIV2_S = 1.5707963f; //smaller, but gives positive cos. (the most precise is too big, this too small)
 		static constexpr float TO_RAD = PI / 180.f;
 		static constexpr float TO_DEG = 180.f / PI;
 		static constexpr float SQR2 = 1.4142135623730950488016887242097f;
