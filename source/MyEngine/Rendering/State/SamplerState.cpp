942745725619a506d35ce6a95d7ef1722efced70
Louis Herman <louisherman@live.be>
2024-09-14 05:47:53 +0200

    EDIT(SamplerState): use mirror address mode by default

--- source/MyEngine/Rendering/State/SamplerState.cpp
+++ source/MyEngine/Rendering/State/SamplerState.cpp
@@ -10,8 +10,8 @@ Rendering::SamplerState::SamplerState()
 	D3D11_SAMPLER_DESC desc{};
 	desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 	desc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-	desc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-	desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	desc.AddressV = D3D11_TEXTURE_ADDRESS_MIRROR;
+	desc.AddressW = D3D11_TEXTURE_ADDRESS_MIRROR;
 	desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
 	const HRESULT result{ Globals::pGpu->GetDevice().CreateSamplerState(&desc, &m_pSamplerState) };
 	if (FAILED(result))
