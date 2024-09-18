80099888fe5d2003eb733b45d8ad75236f21a0b0
Louis Herman <louisherman@live.be>
2024-09-18 22:58:02 +0200

    EDIT(DepthStencilBuffer): option to create as shader-resource

--- source/MyEngine/Rendering/State/DepthStencilBuffer.cpp
+++ source/MyEngine/Rendering/State/DepthStencilBuffer.cpp
@@ -3,18 +3,18 @@
 
 using namespace Rendering;
 
-bool DepthStencilBuffer::Init(const Int2& size)
+bool DepthStencilBuffer::Init(const Int2& size, bool asShaderResource)
 {
 	bool success{ true };
 
 	//Texture
-	ID3D11Texture2D* pTexture{ MakeTexture(size) };
+	ID3D11Texture2D* pTexture{ MakeTexture(size, asShaderResource) };
 	if (!pTexture)
 		return false;
 
 	//View
 	D3D11_DEPTH_STENCIL_VIEW_DESC desc{};
-	desc.Format = TEXTURE_FORMAT;
+	desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
 	desc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
 	desc.Texture2D.MipSlice = 0;
 	const HRESULT result{
@@ -45,7 +45,7 @@ DepthStencilBuffer::~DepthStencilBuffer()
 	SAFE_RELEASE(m_pView);
 }
 
-ID3D11Texture2D* DepthStencilBuffer::MakeTexture(const Int2& size)
+ID3D11Texture2D* DepthStencilBuffer::MakeTexture(const Int2& size, bool asShaderResource)
 {
 	ID3D11Texture2D* pTexture{ nullptr };
 	D3D11_TEXTURE2D_DESC desc{};
@@ -53,11 +53,13 @@ ID3D11Texture2D* DepthStencilBuffer::MakeTexture(const Int2& size)
 	desc.Height = size.y;
 	desc.MipLevels = 1;
 	desc.ArraySize = 1;
-	desc.Format = TEXTURE_FORMAT;
+	desc.Format = asShaderResource ? DXGI_FORMAT_R24G8_TYPELESS : DXGI_FORMAT_D24_UNORM_S8_UINT;
 	desc.SampleDesc.Count = 1;
 	desc.SampleDesc.Quality = 0;
 	desc.Usage = D3D11_USAGE_DEFAULT;
 	desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	if (asShaderResource)
+		desc.BindFlags |= D3D11_BIND_SHADER_RESOURCE;
 	desc.CPUAccessFlags = 0;
 	desc.MiscFlags = 0;
 	const HRESULT result{
--- source/MyEngine/Rendering/State/DepthStencilBuffer.h
+++ source/MyEngine/Rendering/State/DepthStencilBuffer.h
@@ -10,7 +10,7 @@ class DepthStencilBuffer
 {
 public:
 	DepthStencilBuffer() = default;
-	bool Init(const Int2& size);
+	bool Init(const Int2& size, bool asShaderResource = false);
 	bool Update(const Int2& size);
 	void Clear();
 	~DepthStencilBuffer();
@@ -19,10 +19,9 @@ public:
 	const ID3D11DepthStencilView* GetView() const { return m_pView; }
 
 private:
-	static const DXGI_FORMAT TEXTURE_FORMAT{ DXGI_FORMAT_D24_UNORM_S8_UINT };
 	ID3D11DepthStencilView* m_pView{ nullptr };
 
-	static ID3D11Texture2D* MakeTexture(const Int2& size);
+	static ID3D11Texture2D* MakeTexture(const Int2& size, bool asShaderResource);
 };
 }
 }
