eb66ed71a638d5dfde777984fa55cba108dc4988
Louis Herman <louisherman@live.be>
2024-10-01 04:12:51 +0200

    ADD(PrimitiveTopology): constexpr ToDx()

--- source/MyEngine/Rendering/State/PrimitiveTopology.h
+++ source/MyEngine/Rendering/State/PrimitiveTopology.h
@@ -10,6 +10,8 @@ namespace MyEngine
 		class PrimitiveTopology
 		{
 		public:
+			template<ModelTopology TTopology>
+			static constexpr D3D11_PRIMITIVE_TOPOLOGY ToDx();
 			static D3D11_PRIMITIVE_TOPOLOGY ToDx(ModelTopology modelTopology);
 			static void Activate(D3D11_PRIMITIVE_TOPOLOGY topology);
 			static void Activate(ModelTopology modelTopology);
@@ -23,5 +25,26 @@ namespace MyEngine
 			D3D11_PRIMITIVE_TOPOLOGY m_DxTopology;
 			ModelTopology m_ModelTopology;
 		};
+		template<ModelTopology TTopology>
+		inline constexpr D3D11_PRIMITIVE_TOPOLOGY PrimitiveTopology::ToDx()
+		{
+			if constexpr(TTopology == ModelTopology::LineList
+				|| TTopology == ModelTopology::LineListIdx)
+				return D3D11_PRIMITIVE_TOPOLOGY_LINELIST;
+
+			if constexpr (TTopology == ModelTopology::LineStrip
+				|| TTopology == ModelTopology::LineStripIdx)
+				return D3D11_PRIMITIVE_TOPOLOGY_LINESTRIP;
+
+			if constexpr (TTopology == ModelTopology::TriangleList
+				|| TTopology == ModelTopology::TriangleListIdx)
+				return D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+			if constexpr (TTopology == ModelTopology::TriangleStrip
+				|| TTopology == ModelTopology::TriangleStripIdx)
+				return D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP;
+
+			return D3D11_PRIMITIVE_TOPOLOGY_UNDEFINED;
+		}
 	}
 }
