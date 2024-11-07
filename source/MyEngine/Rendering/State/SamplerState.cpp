e18ee02480988f083bcfcf7b3ea82b94d4b16f9b
Louis Herman <louisherman@live.be>
2024-11-07 07:44:11 +0100

    EDIT(SamplerState): option to change address mode

--- source/MyEngine/Rendering/State/SamplerState.cpp
+++ source/MyEngine/Rendering/State/SamplerState.cpp
@@ -5,36 +5,87 @@
 
 #include "../Gpu.h"
 
-Rendering::SamplerState::SamplerState()
+using namespace Rendering;
+
+SamplerState::SamplerState(BorderMode borderX, BorderMode borderY)
 {
-	D3D11_SAMPLER_DESC desc{};
-	desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
-	desc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-	desc.AddressV = D3D11_TEXTURE_ADDRESS_MIRROR;
-	desc.AddressW = D3D11_TEXTURE_ADDRESS_MIRROR;
-	desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-	const HRESULT result{ Globals::pGpu->GetDevice().CreateSamplerState(&desc, &m_pSamplerState) };
-	if (FAILED(result))
-		Logger::PrintError("Failed creating samplerState");
+	const D3D11_SAMPLER_DESC desc{ MakeDesc(borderX, borderY) };
+	MakeDx(desc);
 }
 
-Rendering::SamplerState::~SamplerState()
+SamplerState::SamplerState(BorderMode borderX, BorderMode borderY, Float4 value)
+{
+	D3D11_SAMPLER_DESC desc{ MakeDesc(borderX, borderY) };
+	desc.BorderColor[0] = value.x;
+	desc.BorderColor[1] = value.y;
+	desc.BorderColor[2] = value.z;
+	desc.BorderColor[3] = value.w;
+	MakeDx(desc);
+}
+
+SamplerState::~SamplerState()
 {
 	m_pSamplerState->Release();
 }
 
-void Rendering::SamplerState::ActivateVs() const
+void SamplerState::ActivateVs() const
 {
 	Globals::pGpu->GetContext().VSSetSamplers(0, 1, &m_pSamplerState);
 }
 
-void Rendering::SamplerState::ActivatePs() const
+void SamplerState::ActivatePs() const
 {
 	Globals::pGpu->GetContext().PSSetSamplers(0, 1, &m_pSamplerState);
 }
 
-void Rendering::SamplerState::Activate() const
+void SamplerState::Activate() const
 {
 	Globals::pGpu->GetContext().PSSetSamplers(0, 1, &m_pSamplerState);
 	Globals::pGpu->GetContext().VSSetSamplers(0, 1, &m_pSamplerState);
+}
+
+constexpr D3D11_TEXTURE_ADDRESS_MODE SamplerState::ToDx(BorderMode mode)
+{
+	if (mode == BorderMode::Repeat)
+		return D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_WRAP;
+	if (mode == BorderMode::Mirror)
+		return D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_MIRROR;
+	if (mode == BorderMode::Clamp)
+		return D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_CLAMP;
+	if (mode == BorderMode::Value)
+		return D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_BORDER;
+	if (mode == BorderMode::MirrorOnce)
+		return D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_MIRROR_ONCE;
+}
+
+constexpr SamplerState::BorderMode SamplerState::ToBorderMode(D3D11_TEXTURE_ADDRESS_MODE mode)
+{
+	if (mode == D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_WRAP)
+		return BorderMode::Repeat;
+	if (mode == D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_MIRROR)
+		return BorderMode::Mirror;
+	if (mode == D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_CLAMP)
+		return BorderMode::Clamp;
+	if (mode == D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_BORDER)
+		return BorderMode::Value;
+	if (mode == D3D11_TEXTURE_ADDRESS_MODE::D3D11_TEXTURE_ADDRESS_MIRROR_ONCE)
+		return BorderMode::MirrorOnce;
+}
+
+constexpr D3D11_SAMPLER_DESC SamplerState::MakeDesc(BorderMode borderX, BorderMode borderY)
+{
+	D3D11_SAMPLER_DESC desc{};
+	desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+	desc.AddressU = ToDx(borderX);
+	desc.AddressV = ToDx(borderY);
+	desc.AddressW = D3D11_TEXTURE_ADDRESS_MIRROR;
+	desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	return desc;
+}
+
+void SamplerState::MakeDx(const D3D11_SAMPLER_DESC& desc)
+{
+	const HRESULT result{ Globals::pGpu->GetDevice().CreateSamplerState(&desc, &m_pSamplerState) };
+	if (FAILED(result))
+		Logger::PrintError("Failed creating samplerState");
 }
--- source/MyEngine/Rendering/State/SamplerState.h
+++ source/MyEngine/Rendering/State/SamplerState.h
@@ -4,26 +4,41 @@ struct ID3D11SamplerState;
 
 namespace MyEngine
 {
-	namespace Rendering
-	{
-		class Gpu;
+namespace Rendering
+{
+class Gpu;
 
-		class SamplerState
-		{
-		public:
-			SamplerState();
-			~SamplerState();
-			SamplerState(const SamplerState& other) = delete;
-			SamplerState(SamplerState&& other) noexcept = delete;
-			SamplerState& operator=(const SamplerState& other) = delete;
-			SamplerState& operator=(SamplerState&& other) noexcept = delete;
+class SamplerState
+{
+public:
+	enum class BorderMode {
+		Repeat, //1.2 -> 0.2
+		Mirror,  //1.2 -> 0.8
+		Clamp, //1.2 -> 1.0
+		Value,
+		MirrorOnce //Mirror then Clamp
+	};
 
-			void ActivateVs() const;
-			void ActivatePs() const;
-			void Activate() const;
+	SamplerState(BorderMode borderX = BorderMode::Repeat, BorderMode borderY = BorderMode::Repeat);
+	SamplerState(BorderMode borderX, BorderMode borderY, Float4 value);
+	~SamplerState();
+	SamplerState(const SamplerState& other) = delete;
+	SamplerState(SamplerState&& other) noexcept = delete;
+	SamplerState& operator=(const SamplerState& other) = delete;
+	SamplerState& operator=(SamplerState&& other) noexcept = delete;
 
-		private:
-			ID3D11SamplerState* m_pSamplerState{};
-		};
-	}
+	void ActivateVs() const;
+	void ActivatePs() const;
+	void Activate() const;
+
+private:
+	ID3D11SamplerState* m_pSamplerState{};
+
+	static constexpr D3D11_TEXTURE_ADDRESS_MODE ToDx(BorderMode mode);
+	static constexpr BorderMode ToBorderMode(D3D11_TEXTURE_ADDRESS_MODE mode);
+
+	static constexpr D3D11_SAMPLER_DESC MakeDesc(BorderMode borderX, BorderMode borderY);
+	void MakeDx(const D3D11_SAMPLER_DESC& desc);
+};
+}
 }
