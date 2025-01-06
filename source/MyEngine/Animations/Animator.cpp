1607bf98a7b5fda13cd51b04cb42a137e6e6d31e
Louis Herman <louisherman@live.be>
2025-01-06 20:30:30 +0100

    ADD(Animator): ProgressTimeUntil() & SetDuration()

--- source/MyEngine/Animations/Animator.cpp
+++ source/MyEngine/Animations/Animator.cpp
@@ -25,6 +25,52 @@ void Animator::ProgressTime(const Animation& animation, float time)
 	animation.UpdateModelBuffer(m_Time, m_Buffer, m_Cache);
 }
 
+void Animator::ProgressTimeUntil(const Animation& animation, float limit)
+{
+	ProgressTimeUntil(animation, limit, DELTA_TIME);
+}
+
+void Animator::ProgressTimeUntil(const Animation& animation, float limit, float amount)
+{
+	while (amount >= 1.f)
+		amount -= 1.f;
+
+	if (limit == 0 || limit == 1)
+	{
+		if (m_Time == 0)
+			return;
+
+		m_Time += amount * m_TimeScale;
+
+		if (m_Time >= 1.f)
+			m_Time = 0;
+	}
+	else
+	{
+		if (m_Time == limit)
+			return;
+
+		if (m_Time < limit)
+		{
+			m_Time += amount * m_TimeScale;
+			if (m_Time > limit)
+				m_Time = limit;
+		}
+		else
+		{
+			m_Time += amount * m_TimeScale;
+			if (m_Time >= 1.f)
+			{
+				m_Time -= 1.f;
+				if (m_Time > limit)
+					m_Time = limit;
+			}
+		}
+	}
+
+	animation.UpdateModelBuffer(m_Time, m_Buffer, m_Cache);
+}
+
 void Animator::SetTime(const Animation& animation, float time)
 {
 	SetNormalizedTime(animation, time * m_TimeScale);
@@ -33,4 +79,14 @@ void Animator::SetTime(const Animation& animation, float time)
 void Animator::SetNormalizedTime(const Animation& animation, float time)
 {
 	animation.UpdateModelBuffer(time, m_Buffer, m_Cache);
+}
+
+void Animator::SetDuration(float duration)
+{
+	m_TimeScale = 1.f / duration;
+}
+
+void Animator::SetTimeScale(float timeScale)
+{
+	m_TimeScale = timeScale;
 }
--- source/MyEngine/Animations/Animator.h
+++ source/MyEngine/Animations/Animator.h
@@ -15,10 +15,17 @@ public:
 	void ProgressTime(const Animation& animation);
 	void ProgressTime(const Animation& animation, float time);
 
+	void ProgressTimeUntil(const Animation& animation, float limit);
+	void ProgressTimeUntil(const Animation& animation, float limit, float amount);
+
 	void SetTime(const Animation& animation, float time);
 	void SetNormalizedTime(const Animation& animation, float time);
 
 	const Animation::BonesBuffer& GetBonesBuffer() const { return m_Buffer; }
+	const float GetNormalizedTime() const { return m_Time; }
+
+	void SetDuration(float duration);
+	void SetTimeScale(float timeScale);
 
 private:
 	Animation::BonesBuffer m_Buffer;
