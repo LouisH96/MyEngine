df092852f97963adfaf03451c9aef7c177c83f51
Louis Herman <louisherman@live.be>
2025-02-24 21:53:50 +0100

    ADD(Color.h): some colors

---
+++ source/MyEngine/Other/Color.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <Math\Vectors.h>
+
+namespace MyEngine
+{
+namespace Color
+{
+static const Float3 Red{ 1,0,0 };
+static const Float3 Green{ 0,1,0 };
+static const Float3 Blue{ 0,0,1 };
+
+namespace Chill
+{
+static constexpr float High{ .65f };
+static constexpr float Low{ .25f };
+
+static const Float3 Red{ High, Low, Low };
+static const Float3 Green{ Low, High, Low };
+static const Float3 Blue{ Low, Low, High };
+
+} //end namespace Chill
+
+}
+}
+
