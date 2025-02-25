2ac5873adfbe9684d3d8a9c929db892b1393715d
Louis Herman <louisherman@live.be>
2025-02-25 02:41:27 +0100

    ADD(Colors): Yellow

--- source/MyEngine/Other/Color.h
+++ source/MyEngine/Other/Color.h
@@ -9,6 +9,8 @@ static const Float3 Red{ 1,0,0 };
 static const Float3 Green{ 0,1,0 };
 static const Float3 Blue{ 0,0,1 };
 
+static const Float3 Yellow{ 1,1,0 };
+
 namespace Chill
 {
 static constexpr float High{ .65f };
@@ -17,6 +19,7 @@ static constexpr float Low{ .25f };
 static const Float3 Red{ High, Low, Low };
 static const Float3 Green{ Low, High, Low };
 static const Float3 Blue{ Low, Low, High };
+static const Float3 Yellow{ High, High, Low };
 
 } //end namespace Chill
 
