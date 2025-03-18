9cb52ef97289889ef9eb887ac3cdbf011bffee71
Louis Herman <louisherman@live.be>
2025-03-18 20:28:15 +0100

    ADD(Ray): constructors

--- source/MyEngine/Geometry/Shapes/Ray.cpp
+++ source/MyEngine/Geometry/Shapes/Ray.cpp
@@ -1,6 +1,19 @@
 #include "pch.h"
 #include "Ray.h"
 
+Ray::Ray(const Float3& origin, const Float3& direction, float length)
+	: Origin{ origin }
+	, Direction{ direction }
+	, Length{ length }
+{
+}
+
+Ray::Ray(const Float3& from, const Float3& to)
+	: Origin{ from }
+{
+	Direction = (to - from).Normalized(Length);
+}
+
 Float3 Ray::GetPoint(float distance) const
 {
 	return Origin + Direction * distance;
--- source/MyEngine/Geometry/Shapes/Ray.h
+++ source/MyEngine/Geometry/Shapes/Ray.h
@@ -6,10 +6,12 @@ namespace MyEngine
 	{
 	public:
 		Ray() = default;
+		Ray(const Float3& origin, const Float3& direction, float length);
+		Ray(const Float3& from, const Float3& to);
 
 		Float3 Origin;
 		Float3 Direction;
-		float Length;
+		float Length{};
 
 		Float3 GetPoint(float distance) const;
 		Float3 GetEnd() const;
