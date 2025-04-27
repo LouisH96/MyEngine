3b0ca0b1b5d6cb55dfafc237d932f4cd429600dc
Louis Herman <louisherman@live.be>
2025-04-27 18:32:42 +0200

    EDIT(SphereTriangleCollision): return Result structs & create ContinueDetect function & MoveOut(..) function

--- source/MyEngine/Physics/Collisions/SphereTriangleCollision.cpp
+++ source/MyEngine/Physics/Collisions/SphereTriangleCollision.cpp
@@ -35,16 +35,67 @@ bool SphereTriangleCollision::Detect_Rough(
 	return false;
 }
 
+float SphereTriangleCollision::MoveOut(
+	const Sphere& sphere,
+	const Float3& direction,
+	PtrRangeConst<Float3> points,
+	PtrRangeConst<Float3> triangleNormals)
+{
+	float t{ 0 };
+	Sphere localSphere{ sphere };
+	unsigned iLast{ 0 };
+	unsigned nextILast{ triangleNormals.count };
+	Float3 closest;
+
+	while (iLast != nextILast)
+	{
+		iLast = nextILast;
+		for (
+			unsigned iPoint{ 2 }, iTriangle{ 0 };
+			iTriangle < iLast;
+			iPoint += 3, ++iTriangle)
+		{
+			const Float3* pPoint{ &points[iPoint - 2] };
+
+			if (!Detect(localSphere, pPoint, closest))
+				continue;
+
+			if (closest.DistanceSq(localSphere.GetCenter()) + Constants::PHYSICS_EPSILON > localSphere.GetRadiusSq())
+				continue;
+
+			const float dot{
+				direction.Dot(localSphere.GetCenter() - closest)
+			};
+			const Float3 perpendicular{
+				closest + direction * dot
+			};
+
+			const float centerToEdge{
+				sqrtf(localSphere.GetRadiusSq() - perpendicular.DistanceSq(localSphere.GetCenter()))
+			};
+			const float distance{
+				centerToEdge - dot
+			};
+
+			t += distance + Constants::PHYSICS_EPSILON;
+			localSphere.SetCenter(sphere.GetCenter() + direction * t);
+			nextILast = iTriangle;
+			iLast = triangleNormals.count;
+		}
+	}
+	return t;
+}
+
 bool SphereTriangleCollision::Detect(
-	const Sphere& sphere, const List<Float3>& points)
+	const Sphere& sphere, const List<Float3>& points,
+	Float3& closest)
 {
 	const float radiusSq{ sphere.GetRadiusSq() };
 	for (unsigned iPoint{ 2 }; iPoint < points.GetSize(); iPoint += 3)
 	{
-		const Float3 closest{ PointTriangleCollision::Closest(
+		closest = PointTriangleCollision::Closest(
 			sphere.GetCenter(),
-			&points[iPoint - 2])
-		};
+			&points[iPoint - 2]);
 
 		const Float3 v{ closest - sphere.GetCenter() };
 		if (v.LengthSq() <= radiusSq)
@@ -53,12 +104,14 @@ bool SphereTriangleCollision::Detect(
 	return false;
 }
 
-bool SphereTriangleCollision::Detect(const Sphere& sphere, const Float3* pPoints)
+bool SphereTriangleCollision::Detect(
+	const Sphere& sphere,
+	const Float3* pPoints,
+	Float3& closest)
 {
-	const Float3 closest{ PointTriangleCollision::Closest(
+	closest = PointTriangleCollision::Closest(
 		sphere.GetCenter(),
-		pPoints)
-	};
+		pPoints);
 
 	const Float3 v{ closest - sphere.GetCenter() };
 	if (v.LengthSq() <= sphere.GetRadiusSq())
@@ -66,61 +119,67 @@ bool SphereTriangleCollision::Detect(const Sphere& sphere, const Float3* pPoints
 	return false;
 }
 
+bool SphereTriangleCollision::Detect(
+	const Sphere& sphere, const Float3& direction,
+	PtrRangeConst<Float3> points, PtrRangeConst<Float3> triangleNormals,
+	MovingSphereCollision& result)
+{
+	result.T = nextafterf(sphere.GetRadiusSq(), Float::MAX);
+	return ContinueDetecting(
+		sphere, direction,
+		points, triangleNormals,
+		result);;
+}
+
 /*
 	Real-Time Collision Detection book
 		5.5.6 Intersecting Moving Sphere Against Triangle (and Polygon)(p.226)
-
-	Need some improvement. For example if the sphere starts inside the triangle.
 */
-bool SphereTriangleCollision::Detect(
-	const Sphere& sphere, const Float3& direction, float length,
-	PtrRangeConst<Float3> points, PtrRangeConst<Float3> normals)
+bool SphereTriangleCollision::ContinueDetecting(
+	const Sphere& sphere, const Float3& direction,
+	PtrRangeConst<Float3> points, PtrRangeConst<Float3> triangleNormals,
+	MovingSphereCollision& result)
 {
-	float closestT{ std::nextafterf(length, Float::MAX) };
-	unsigned closestTriangle{ Uint::MAX };
+	const float initT{ result.T };
 
 	for (unsigned iPoint{ 2 }, iTriangle{ 0 };
 		iPoint < points.count;
 		iPoint += 3, iTriangle++)
 	{
 		const Float3* pTriangle{ &points[iPoint - 2] };
-		const Float3& normal{ normals[iTriangle] };
+		const Float3& normal{ triangleNormals[iTriangle] };
 
-		if (SphereTriangleCollision::Detect(sphere, pTriangle))
-		{
-			closestT = 0;
-			closestTriangle = iTriangle;
-			return true;
-		}
+		if (normal.Dot(direction) > 0)
+			continue;
 
-		const Float3 d{ sphere.GetCenter() - normal * sphere.GetRadius() }; //point on sphere that will hit plane first
+		const Float3 sphereFirstHit{
+			sphere.GetCenter() - normal * sphere.GetRadius() }; //point on sphere that will hit plane first
 
 		float t;
 		LinePlaneCollision::Detect(
-			d, direction, points[iPoint], normal, t);
+			sphereFirstHit, direction, points[iPoint], normal, t);
 
-		if (t < 0 || t >= closestT)
+		if (t < 0 || t >= result.T)
 			continue;
 
-		const Float3 p{ d + direction * t }; //point on plane that will be hit first
+		const Float3 planeFirstHit{
+			sphereFirstHit + direction * t }; //point on plane that will be hit first
 
-		Float3 q{}; //closest triangle point to p
-		if (PointTriangleCollision::DetectOrClosest(p, pTriangle, q))
+		Float3 triangleClosest{}; //closest triangle point (to planeFirstHit)
+		if (PointTriangleCollision::DetectOrClosest(planeFirstHit, pTriangle, triangleClosest))
 		{
-			closestT = t;
-			closestTriangle = iTriangle;
+			result.HitDirection = -normal;
+			result.T = t;
 			continue;
 		}
 
-		Ray returnRay{ q, -direction, closestT };
+		Ray returnRay{ triangleClosest, -direction, result.T };
 		if (LineSphereCollision::Detect(returnRay, sphere, t))
 		{
-			closestT = t;
-			closestTriangle = iTriangle;
+			result.HitDirection = triangleClosest - (sphere.GetCenter() + direction * t);
+			result.T = t;
 		}
 	}
 
-	if (closestTriangle == Uint::MAX)
-		return false;
-	return true;
+	return result.T < initT;
 }
--- source/MyEngine/Physics/Collisions/SphereTriangleCollision.h
+++ source/MyEngine/Physics/Collisions/SphereTriangleCollision.h
@@ -4,9 +4,7 @@
 #include <Geometry\Shapes\Sphere.h>
 #include <Geometry\Shapes\Triangle.h>
 
-namespace MyEngine
-{
-namespace Physics
+namespace MyEngine::Physics
 {
 /*
 	Detect_Rough simply checks if one of the corners is inside the sphere.
@@ -20,7 +18,9 @@ public:
 		//A
 		const Sphere& sphere,
 		//B
-		const Float3* pPoints);
+		const Float3* pPoints,
+		//Result
+		Float3& closest);
 
 	static bool Detect_Rough(
 		//A
@@ -33,7 +33,9 @@ public:
 		//A
 		const Sphere& sphere,
 		//B
-		const List<Float3>& points);
+		const List<Float3>& points,
+		//Result
+		Float3& closest);
 
 	static bool Detect_Rough(
 		//A
@@ -41,32 +43,68 @@ public:
 		//B
 		const List<Float3>& points);
 
+	static float MoveOut(
+		//A
+		const Sphere& sphere,
+		const Float3& direction,
+		//B
+		PtrRangeConst<Float3> points, PtrRangeConst<Float3> triangleNormals);
+
 	//---| Moving Sphere |---
 
 	//--| Sphere, Direction, Length |-VS-| Points, TriangleNormals |--
+	struct MovingSphereCollision
+	{
+		Float3 HitDirection;
+		float T;
+	};
+
+	/*
+		Does not detect triangles starting inside the sphere! (by design)
+		Result::HitDirection might not be normalized!
+	*/
 	static bool Detect(
 		//A
-		const Sphere& sphere, const Float3& direction, float length,
+		const Sphere& sphere, const Float3& direction,
 		//B
-		PtrRangeConst<Float3> points, PtrRangeConst<Float3> triangleNormals);
+		PtrRangeConst<Float3> points, PtrRangeConst<Float3> triangleNormals,
+		//Result
+		MovingSphereCollision& result
+	);
+	/*
+		Difference with Detect is this one takes
+			an active/initialized Result struct.
+	*/
+	static bool ContinueDetecting(
+		//A
+		const Sphere& sphere, const Float3& direction,
+		//B
+		PtrRangeConst<Float3> points, PtrRangeConst<Float3> triangleNormals,
+		//Result
+		MovingSphereCollision& result
+	);
 
 	template<typename TPoints, typename TNormals>
 	static bool Detect(
 		//A
 		const Sphere& sphere, const Float3& direction, float length,
 		//B
-		const TPoints& points, const TNormals& triangleNormals);
+		const TPoints& points, const TNormals& triangleNormals,
+		//Result
+		MovingSphereCollision& result);
 };
 
 template<typename TPoints, typename TNormals>
 inline bool SphereTriangleCollision::Detect(
 	const Sphere& sphere, const Float3& direction, float length,
-	const TPoints& points, const TNormals& normals)
+	const TPoints& points, const TNormals& normals,
+	//Result
+	MovingSphereCollision& result)
 {
 	return Detect(
 		sphere, direction, length,
-		PtrRangeConst<Float3>{points}, PtrRangeConst<Float3>{normals});
+		PtrRangeConst<Float3>{points}, PtrRangeConst<Float3>{normals},
+		result);
 }
 
-}
 }
