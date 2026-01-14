3838f906fd8d0950e080ba0ec85bfaeadb52b8bf
Louis Herman <louisherman@live.be>
2026-01-14 15:54:59 +0100

    ADD(Math.h): Unlerp(..)

--- source/MyBasics/Math/Math.h
+++ source/MyBasics/Math/Math.h
@@ -7,10 +7,20 @@ class Math
 public:
 	template<typename T>
 	static T Lerp(float alpha, const T& v0, const T& v1);
+
+	template<typename T>
+	static T Unlerp(const T& min, const T& max, const T& value);
 };
+
 template<typename T>
 inline T Math::Lerp(float alpha, const T& v0, const T& v1)
 {
 	return (v1 - v0) * alpha + v0;
+}
+
+template<typename T>
+inline T Math::Unlerp(const T& min, const T& max, const T& value)
+{
+	return (value - min) / (max - min);
 }
 }
