eed47bc83880c8fae9684509ccdd45d8ea8c8f57
Louis Herman <louisherman@live.be>
2026-01-14 16:02:35 +0100

    FIX(InvalidateBuffer): append commit

--- source/MyEngine/Rendering/Buffers/InvalidateBuffer.h
+++ source/MyEngine/Rendering/Buffers/InvalidateBuffer.h
@@ -4,98 +4,110 @@
 
 namespace MyEngine
 {
-	namespace Rendering
+template<typename Data>
+class InvalidateBuffer
+{
+public:
+	//---| Construction |---
+	InvalidateBuffer() = default;
+	explicit InvalidateBuffer(unsigned initCapacity);
+
+	//---| Functions |---
+	unsigned Add(const Data& data);
+	int Add(Data&& data);
+	Data Remove(int idx);
+
+	const InvalidateList<Data>& GetCpuData() const { return m_CpuBuffer; }
+	InvalidateList<Data>& GetCpuData() { return m_CpuBuffer; }
+
+	Data& Get(unsigned idx) { return m_CpuBuffer.Get(idx); }
+
+	unsigned GetCount() const { return m_CpuBuffer.GetSize(); }
+	const Data* GetFirst() const { return m_CpuBuffer.GetFirst(); }
+	const Data* GetLast() const { return m_CpuBuffer.GetLast(); }
+	const Data* GetEnd() const { return m_CpuBuffer.GetEnd(); }
+	Data* GetFirst() { return m_CpuBuffer.GetFirst(); }
+	Data* GetLast() { return m_CpuBuffer.GetLast(); }
+	Data* GetEnd() { return m_CpuBuffer.GetEnd(); }
+
+	const Data& operator[](int idx) const { return m_CpuBuffer.GetData()[idx]; }
+	Data& operator[](int idx) { return m_CpuBuffer.GetData()[idx]; }
+
+	void ActivateVertexBuffer(unsigned slot);
+	void ActivateFromStart(unsigned slot = 0);
+	void Draw() const;
+	void Draw(unsigned nrVertices) const;
+	void DrawIdx(unsigned nrIndices) const;
+
+private:
+	Rendering::Buffer<Data> m_GpuBuffer;
+	InvalidateList<Data> m_CpuBuffer;
+};
+
+template <typename Data>
+InvalidateBuffer<Data>::InvalidateBuffer(unsigned initCapacity)
+	: m_GpuBuffer{ initCapacity, true }
+	, m_CpuBuffer{ initCapacity }
+{
+}
+
+template <typename Data>
+unsigned InvalidateBuffer<Data>::Add(const Data& data)
+{
+	return m_CpuBuffer.Add(data);
+}
+
+template <typename Data>
+int InvalidateBuffer<Data>::Add(Data&& data)
+{
+	return m_CpuBuffer.Add(std::move(data));
+}
+
+template <typename Data>
+Data InvalidateBuffer<Data>::Remove(int idx)
+{
+	return m_CpuBuffer.Remove(idx);
+}
+
+template <typename Data>
+void InvalidateBuffer<Data>::ActivateVertexBuffer(unsigned slot)
+{
+	if (m_CpuBuffer.HasChanged())
 	{
-		template<typename Data>
-		class InvalidateBuffer
-		{
-		public:
-			//---| Construction |---
-			InvalidateBuffer() = default;
-			explicit InvalidateBuffer(unsigned initCapacity);
-
-			//---| Functions |---
-			unsigned Add(const Data& data);
-			int Add(Data&& data);
-			Data Remove(int idx);
-
-			const InvalidateList<Data>& GetCpuData() const { return m_CpuBuffer; }
-			InvalidateList<Data>& GetCpuData() { return m_CpuBuffer; }
-
-			unsigned GetCount() const { return m_CpuBuffer.GetSize(); }
-			const Data* GetFirst() const { return m_CpuBuffer.GetFirst(); }
-			const Data* GetLast() const { return m_CpuBuffer.GetLast(); }
-			const Data* GetEnd() const { return m_CpuBuffer.GetEnd(); }
-			Data* GetFirst() { return m_CpuBuffer.GetFirst(); }
-			Data* GetLast() { return m_CpuBuffer.GetLast(); }
-			Data* GetEnd() { return m_CpuBuffer.GetEnd(); }
-
-			const Data& operator[](int idx) const { return m_CpuBuffer.GetData()[idx]; }
-			Data& operator[](int idx) { return m_CpuBuffer.GetData()[idx]; }
-
-			void ActivateVertexBuffer(unsigned slot);
-			void Draw() const;
-			void Draw(unsigned nrVertices) const;
-			void DrawIdx(unsigned nrIndices) const;
-
-		private:
-			Buffer<Data> m_GpuBuffer;
-			InvalidateList<Data> m_CpuBuffer;
-		};
-
-		template <typename Data>
-		InvalidateBuffer<Data>::InvalidateBuffer(unsigned initCapacity)
-			: m_GpuBuffer{ initCapacity, true }
-			, m_CpuBuffer{ initCapacity }
-		{
-		}
-
-		template <typename Data>
-		unsigned InvalidateBuffer<Data>::Add(const Data& data)
-		{
-			return m_CpuBuffer.Add(data);
-		}
-
-		template <typename Data>
-		int InvalidateBuffer<Data>::Add(Data&& data)
-		{
-			return m_CpuBuffer.Add(std::move(data));
-		}
-
-		template <typename Data>
-		Data InvalidateBuffer<Data>::Remove(int idx)
-		{
-			return m_CpuBuffer.Remove(idx);
-		}
-
-		template <typename Data>
-		void InvalidateBuffer<Data>::ActivateVertexBuffer(unsigned slot)
-		{
-			if (m_CpuBuffer.HasChanged())
-			{
-				m_CpuBuffer.ClearChangedFlag();
-				m_GpuBuffer.EnsureCapacity(m_CpuBuffer.GetCapacity());
-				m_GpuBuffer.CopyData(m_CpuBuffer.SeeFirst(), m_CpuBuffer.GetSize());
-			}
-			m_GpuBuffer.Activate(slot);
-		}
-
-		template <typename Data>
-		void InvalidateBuffer<Data>::Draw() const
-		{
-			Globals::pGpu->GetContext().Draw(m_CpuBuffer.GetSize(), 0);
-		}
-
-		template <typename Data>
-		void InvalidateBuffer<Data>::Draw(unsigned nrVertices) const
-		{
-			Globals::pGpu->GetContext().DrawInstanced(nrVertices, GetCount(), 0, 0);
-		}
-
-		template <typename Data>
-		void InvalidateBuffer<Data>::DrawIdx(unsigned nrIndices) const
-		{
-			Globals::pGpu->GetContext().DrawIndexedInstanced(nrIndices, GetCount(), 0, 0, m_CpuBuffer.GetFirstIdx());
-		}
+		m_CpuBuffer.ClearChangedFlag();
+		m_GpuBuffer.EnsureCapacity(m_CpuBuffer.GetSize());
+		m_GpuBuffer.CopyData(m_CpuBuffer.SeeFirst(), m_CpuBuffer.GetSize());
 	}
+	m_GpuBuffer.Activate(slot);
+}
+
+template<typename Data>
+inline void InvalidateBuffer<Data>::ActivateFromStart(unsigned slot)
+{
+	if (m_CpuBuffer.HasChanged())
+	{
+		m_CpuBuffer.ClearChangedFlag();
+		m_GpuBuffer.EnsureCapacity(m_CpuBuffer.GetEndIdx());
+		m_GpuBuffer.CopyData(m_CpuBuffer.SeeData(), m_CpuBuffer.GetEndIdx());
+	}
+	m_GpuBuffer.Activate(slot);
+}
+
+template <typename Data>
+void InvalidateBuffer<Data>::Draw() const
+{
+	Globals::pGpu->GetContext().Draw(m_CpuBuffer.GetSize(), 0);
+}
+
+template <typename Data>
+void InvalidateBuffer<Data>::Draw(unsigned nrVertices) const
+{
+	Globals::pGpu->GetContext().DrawInstanced(nrVertices, GetCount(), 0, 0);
+}
+
+template <typename Data>
+void InvalidateBuffer<Data>::DrawIdx(unsigned nrIndices) const
+{
+	Globals::pGpu->GetContext().DrawIndexedInstanced(nrIndices, GetCount(), 0, 0, m_CpuBuffer.GetFirstIdx());
+}
 }
